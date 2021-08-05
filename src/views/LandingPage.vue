0acdc512c58e749d491f574d9843c4cb92d3ad47
Kjell Magne Fauske <kjellmf@gmail.com>
2021-08-05 10:33:28 +0200

    feat: add button for creating a new scenario on landing page (#9)
    Closes #9

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,4 +1,3 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <div class="min-h-screen bg-gray-100 overflow-hidden">
     <main>
--- src/views/LandingPageScenarios.vue
+++ src/views/LandingPageScenarios.vue
@@ -46,6 +46,45 @@
             </dl>
           </router-link>
         </li>
+        <li class="col-span-1 flex">
+          <button
+            type="button"
+            @click="newScenario"
+            class="
+              relative
+              block
+              w-full
+              border-2 border-gray-300 border-dashed
+              rounded-lg
+              p-12
+              text-center
+              hover:border-gray-400
+              focus:outline-none
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-indigo-500
+            "
+          >
+            <svg
+              class="mx-auto h-12 w-12 text-gray-400"
+              xmlns="http://www.w3.org/2000/svg"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
+              />
+            </svg>
+            <span class="mt-2 block text-sm font-medium text-gray-900">
+              Create new scenario
+            </span>
+          </button>
+        </li>
       </ul>
     </section>
   </div>
@@ -92,9 +131,14 @@ export default {
     const getScenarioTo = (scenarioId) => {
       return { name: SCENARIO_ROUTE, query: { load: scenarioId } };
     };
+
+    const newScenario = () => {
+      router.push({ name: SCENARIO_ROUTE });
+    };
     return {
       scenarios,
       getScenarioTo,
+      newScenario,
     };
   },
 };
