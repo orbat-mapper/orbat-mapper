4b6bb1283e4e80fa11ae01f516d78257831cfb58
Kjell Magne Fauske <kjellmf@gmail.com>
2021-08-06 19:30:31 +0200

    docs: add link to GitHub repo on landing page and tweak prose

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -4,68 +4,67 @@
       <ProseSection class="">
         <h1><span class="text-red-900">Order of Battle</span> Mapper</h1>
         <p>Recreate historic battles.</p>
+
+        <p>
+          Follow the development of
+          <a href="https://github.com/kjellmf/orbat-mapper"
+            >ORBAT Mapper on GitHub <GithubIcon class="w-10 h-10 inline" /> </a
+          >.
+        </p>
       </ProseSection>
       <LandingPageScenarios />
 
       <ProseSection class="">
-        <h2>Features</h2>
-        <h2>Credits</h2>
+        <!--        <h2>Features</h2>-->
+        <!--        <h2>Credits</h2>-->
       </ProseSection>
 
       <ProseSection class="">
-        <h2>Related tools</h2>
+        <h2>Other tools and resources</h2>
 
         <p>
           ORBAT mapper is created by a hobbyist. If you need something more
-          professional try some of these excellent tools:
-        </p>
-
-        <ul>
-          <li>map.army</li>
-          <li>MGRS mapper</li>
-          <li>ODIN</li>
-        </ul>
-
-        <h2>Tools for creating military overlays</h2>
-        <p>
-          Need to plan your next mission or recreate a historic battle? Here are
-          a few options
-        </p>
-
-        <p>
-          <a href="https://www.map.army/about/en.html">map.army</a> - a really
-          nice web application for drawing map overlays with symbols and
-          tactical graphics.
-        </p>
-
-        <p>
-          <a href="https://mgrs-mapper.com/">MGRS Mapper</a> - operational
-          graphics tool for junior leaders. Subscription based service. A
-          limited free version is available.
-        </p>
-
-        <p>
-          <a href="https://odin.syncpoint.io/en/"
-            >ODIN open source command and control information system</a
-          >. Desktop application available on most operating systems (Electron
-          based). Still under development, but looks very promising!
+          professional or you want to create military overlays, try some of
+          these excellent tools instead:
         </p>
 
         <ul>
           <li>
-            <a href="https://www.symbol.army/about/en.html">symbol.army</a>.
+            <p>
+              <a href="https://www.map.army/about/en.html">map.army</a> - a
+              really nice web application for drawing map overlays with symbols
+              and tactical graphics.
+            </p>
           </li>
+
           <li>
-            <a href="https://odin.syncpoint.io/en/"
-              >ODIN open source command and control information system</a
-            >.
+            <p>
+              <a href="https://mgrs-mapper.com/">MGRS Mapper</a> -
+              <q>operational graphics tool for junior leaders</q>. Subscription
+              based service. A limited free version is available.
+            </p>
           </li>
+
           <li>
-            <a href="https://www.project44.ca/"
-              >Project'44 the road to liberation</a
-            >.
+            <p>
+              <a href="https://odin.syncpoint.io/en/"
+                >ODIN open source command and control information system</a
+              >. Desktop application available on most operating systems
+              (Electron based). Still under development, but looks very
+              promising!
+            </p>
           </li>
         </ul>
+
+        <p>
+          If you are interested in military history I highly recommend the site
+          <a href="https://www.project44.ca/"
+            >Project'44 the road to liberation</a
+          >. Their
+          <a href="https://map.project44.ca/">interactive WWII web map</a> and
+          <a href="https://map.project44.ca/story-maps">story maps</a> are
+          fantastic!
+        </p>
       </ProseSection>
     </main>
   </div>
@@ -74,11 +73,13 @@
 <script>
 import ProseSection from "../components/ProseSection.vue";
 import LandingPageScenarios from "./LandingPageScenarios.vue";
+import { Github as GithubIcon } from "mdue";
 
 export default {
   components: {
     LandingPageScenarios,
     ProseSection,
+    GithubIcon,
   },
   setup() {
     return {};
