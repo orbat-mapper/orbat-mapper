8e1cb3cfc01eeb2042097b2b9a00eb11f4107945
Kjell Magne Fauske <kjellmf@gmail.com>
2021-08-22 10:03:14 +0200

    feat: add setting for changing map symbol size
    Closes #35

--- src/views/StoryModeView.vue
+++ src/views/StoryModeView.vue
@@ -1,7 +1,37 @@
 <template>
   <div class="md:flex md:flex-col md:h-screen">
-    <header class="p-4 w-full bg-gray-100 md:flex-shrink-0">
+    <header class="p-4 w-full bg-gray-100 md:flex-shrink-0 relative">
       <h1>{{ scenario.name }}</h1>
+      <button
+        type="button"
+        @click="toggleSidebar()"
+        class="
+          z-20
+          fixed
+          top-2
+          right-4
+          bg-white bg-opacity-75
+          inline-flex
+          items-center
+          justify-center
+          p-2
+          rounded-md
+          text-gray-500
+          hover:text-gray-500 hover:bg-gray-100
+          focus:outline-none
+          focus:ring-2
+          focus:ring-offset-2
+          focus:ring-indigo-500
+        "
+      >
+        <span class="sr-only">Open main menu</span>
+        <MenuIcon
+          v-if="!sidebarIsOpen"
+          class="block h-6 w-6"
+          aria-hidden="true"
+        />
+        <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+      </button>
     </header>
     <div class="md:flex-auto md:min-h-0 md:flex">
       <section
@@ -26,6 +56,13 @@
         <StoryModeContent @update-state="onUpdateState" />
       </section>
     </div>
+    <SlideOver v-model="sidebarIsOpen" title="Settings">
+      <InputGroup
+        label="Symbol size"
+        type="number"
+        v-model="settingsStore.mapIconSize"
+      ></InputGroup>
+    </SlideOver>
   </div>
 </template>
 
@@ -41,7 +78,7 @@ import {
 } from "vue";
 import MapContainer from "../components/MapContainer.vue";
 import { useScenarioStore } from "../stores/scenarioStore";
-import { and, invoke, toRefs, until, useTitle, whenever } from "@vueuse/core";
+import { and, invoke, useTitle, useToggle, whenever } from "@vueuse/core";
 import { useScenarioIO } from "../stores/scenarioIO";
 import OLMap from "ol/Map";
 import { useUnitLayer } from "../composables/geomap";
@@ -52,16 +89,28 @@ import dayjs from "dayjs";
 import { flyTo } from "../geo/layers";
 import { useSettingsStore } from "../stores/settingsStore";
 import { clearStyleCache } from "../geo/styles";
+import SlideOver from "../components/SlideOver.vue";
+import { MenuIcon, XIcon } from "@heroicons/vue/outline";
+import InputGroup from "../components/InputGroup.vue";
 
 export default defineComponent({
   name: "StoryModeView",
-  components: { StoryModeContent, MapContainer },
+  components: {
+    InputGroup,
+    SlideOver,
+    StoryModeContent,
+    MapContainer,
+    MenuIcon,
+    XIcon,
+  },
   setup() {
     const scenarioStore = useScenarioStore();
     const scenarioIO = useScenarioIO();
     const originalTitle = useTitle().value;
     const windowTitle = computed(() => scenarioStore.scenario.name);
     const mapIsReady = ref(false);
+    const sidebarIsOpen = ref(false);
+    const toggleSidebar = useToggle(sidebarIsOpen);
     let mapInstance: OLMap;
 
     const settingsStore = useSettingsStore();
@@ -143,7 +192,14 @@ export default defineComponent({
       drawUnits();
     });
 
-    return { scenario, onMapReady, onUpdateState };
+    return {
+      scenario,
+      onMapReady,
+      onUpdateState,
+      sidebarIsOpen,
+      toggleSidebar,
+      settingsStore,
+    };
   },
 });
 </script>
