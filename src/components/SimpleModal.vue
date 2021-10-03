d24cc5423e0b68bba3e0e0bc644462394513db4e
Kjell Magne Fauske <kjellmf@gmail.com>
2021-10-03 12:09:58 +0200

    feat: add browse tab to symbol picker

--- src/components/SimpleModal.vue
+++ src/components/SimpleModal.vue
@@ -73,7 +73,7 @@
             "
           >
             <div class="">
-              <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+              <div class="mt-3 text-center sm:mt-0 sm:ml-2 sm:text-left">
                 <DialogTitle
                   as="h3"
                   class="text-lg leading-6 font-medium text-gray-900"
--- src/components/SymbolPickerModal.vue
+++ src/components/SymbolPickerModal.vue
@@ -1,117 +1,179 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <SimpleModal v-model="open" dialog-title="Select symbol">
+  <SimpleModal v-model="open" dialog-title="Symbol picker">
     <header class="mt-4 w-16 h-20">
       <MilSymbol :sidc="csidc" :size="34" />
     </header>
 
-    <SearchModalInput
-      class="pb-3"
-      placeholder="Search for symbol"
-      v-model="searchQuery"
-      @keydown.tab="onTab"
-      @keydown.esc.stop="hitsIsOpen ? (hitsIsOpen = false) : (open = false)"
-      @keydown.enter.prevent="!hitsIsOpen && onSubmit()"
-    />
+    <TabView>
+      <TabItem label="Select" v-slot="{ isActive }">
+        <SearchModalInput
+          class="pb-3"
+          placeholder="Search for symbol"
+          v-model="searchQuery"
+          @keydown.tab="onTab"
+          @keydown.esc.stop="hitsIsOpen ? (hitsIsOpen = false) : (open = false)"
+          @keydown.enter.prevent="!hitsIsOpen && onSubmit()"
+        />
 
-    <div class="relative" v-if="hits?.length && hitsIsOpen" ref="hitsRef">
-      <ul
-        class="
-          absolute
-          z-10
-          mt-1
-          w-full
-          bg-white
-          shadow-2xl
-          max-h-56
-          border border-gray-400
-          rounded-md
-          py-1
-          text-base
-          ring-1 ring-black ring-opacity-5
-          overflow-auto
-          focus:outline-none
-          sm:text-sm
-          divide-y divide-gray-100
-        "
-      >
-        <li
-          v-for="(item, index) in hits"
-          class="
-            text-sm
-            p-2
-            py-3
-            flex
-            items-center
-            cursor-default
-            hover:bg-gray-200
-          "
-          tabindex="-1"
-          :class="{ 'bg-gray-200': index === currentIndex }"
-          :key="item.sidc"
-          :id="item.sidc"
-          @click="onSelect(index)"
-        >
-          <p class="flex-shrink-0 h-7 w-9 flex justify-center">
-            <MilSymbol :size="25" :sidc="item.sidc" />
-          </p>
-          <span class="text-sm ml-3">{{ item.text }} </span>
-        </li>
-      </ul>
-    </div>
-    <form class="space-y-4" @submit.prevent="onSubmit" v-if="isLoaded">
-      <!--      <input type="text" :value="sidc" />-->
-      <SymbolCodeSelect
-        v-model="symbolSetValue"
-        label="Symbol set"
-        :items="symbolSets"
-      />
-      <SymbolCodeSelect
-        v-model="statusValue"
-        label="Status"
-        :items="statusItems"
-      />
-      <SymbolCodeSelect
-        v-model="hqtfdValue"
-        label="Headquaters / Task force / Dummy"
-        :items="hqtfdItems"
-      />
-      <SymbolCodeSelect
-        v-model="emtValue"
-        label="Echelon / Mobility / Towed array"
-        :items="emtItems"
-      />
-      <SymbolCodeMultilineSelect
-        v-model="iconValue"
-        label="Main icon"
-        :items="icons"
-      />
-      <SymbolCodeSelect
-        v-model="mod1Value"
-        label="Modifier 1"
-        :items="mod1Items"
-      />
-      <SymbolCodeSelect
-        v-model="mod2Value"
-        label="Modifier 2"
-        :items="mod2Items"
-      />
-
-      <div style="min-height: 14rem" class="py-4 flex justify-end">
-        <div class="">
-          <PrimaryButton type="submit" class="">Select symbol</PrimaryButton>
+        <div class="relative" v-if="hits?.length && hitsIsOpen" ref="hitsRef">
+          <ul
+            class="
+              absolute
+              z-10
+              mt-1
+              w-full
+              bg-white
+              shadow-2xl
+              max-h-56
+              border border-gray-400
+              rounded-md
+              py-1
+              text-base
+              ring-1 ring-black ring-opacity-5
+              overflow-auto
+              focus:outline-none
+              sm:text-sm
+              divide-y divide-gray-100
+            "
+          >
+            <li
+              v-for="(item, index) in hits"
+              class="
+                text-sm
+                p-2
+                py-3
+                flex
+                items-center
+                cursor-default
+                hover:bg-gray-200
+              "
+              tabindex="-1"
+              :class="{ 'bg-gray-200': index === currentIndex }"
+              :key="item.sidc"
+              :id="item.sidc"
+              @click="onSelect(index)"
+            >
+              <p class="flex-shrink-0 h-7 w-9 flex justify-center">
+                <MilSymbol :size="25" :sidc="item.sidc" />
+              </p>
+              <span class="text-sm ml-3">{{ item.text }} </span>
+            </li>
+          </ul>
         </div>
-      </div>
-    </form>
-    <GlobalEvents
-      v-if="hits?.length && hitsIsOpen"
-      @keydown.arrow-down="doKbd('down')"
-      @keydown.page-down="doKbd('pagedown')"
-      @keydown.arrow-up="doKbd('up')"
-      @keydown.page-up="doKbd('pageup')"
-      @keydown.enter.prevent="onSelect()"
-    >
-    </GlobalEvents>
+        <form class="space-y-4" @submit.prevent="onSubmit" v-if="isLoaded">
+          <!--      <input type="text" :value="sidc" />-->
+          <SymbolCodeSelect
+            v-model="symbolSetValue"
+            label="Symbol set"
+            :items="symbolSets"
+          />
+          <SymbolCodeSelect
+            v-model="statusValue"
+            label="Status"
+            :items="statusItems"
+          />
+          <SymbolCodeSelect
+            v-model="hqtfdValue"
+            label="Headquaters / Task force / Dummy"
+            :items="hqtfdItems"
+          />
+          <SymbolCodeSelect
+            v-model="emtValue"
+            label="Echelon / Mobility / Towed array"
+            :items="emtItems"
+          />
+          <SymbolCodeMultilineSelect
+            v-model="iconValue"
+            label="Main icon"
+            :items="icons"
+          />
+          <SymbolCodeSelect
+            v-model="mod1Value"
+            label="Modifier 1"
+            :items="mod1Items"
+          />
+          <SymbolCodeSelect
+            v-model="mod2Value"
+            label="Modifier 2"
+            :items="mod2Items"
+          />
+
+          <div style="min-height: 14rem" class="py-4 flex justify-end">
+            <div class="">
+              <PrimaryButton type="submit" class=""
+                >Select symbol
+              </PrimaryButton>
+            </div>
+          </div>
+        </form>
+        <GlobalEvents
+          v-if="isActive && hits?.length && hitsIsOpen"
+          @keydown.arrow-down="doKbd('down')"
+          @keydown.page-down="doKbd('pagedown')"
+          @keydown.arrow-up="doKbd('up')"
+          @keydown.page-up="doKbd('pageup')"
+          @keydown.enter.prevent="onSelect()"
+        >
+        </GlobalEvents>
+      </TabItem>
+      <TabItem label="Browse" v-slot="{ isActive }">
+        <div class="min-h-[30rem]" v-if="isActive">
+          <SymbolCodeSelect
+            v-model="symbolSetValue"
+            label="Symbol set"
+            :items="symbolSets"
+          />
+
+          <div class="overflow-auto max-h-[40rem] mt-2">
+            <div
+              v-for="[entity, entityIcons] in iconsByEntity"
+              class="relative"
+            >
+              <p class="text-gray-900 font-medium bg-gray-200 p-2 sticky top-0">
+                {{ entity }}
+              </p>
+              <div class="mt-4 grid grid-cols-3 gap-x-2 gap-y-4 p-1">
+                <button
+                  type="button"
+                  v-for="{
+                    sidc,
+                    entity,
+                    entityType,
+                    entitySubtype,
+                    code,
+                  } in entityIcons"
+                  :key="sidc"
+                  @click="iconValue = code"
+                  class="
+                    border border-transparent
+                    rounded
+                    p-4
+                    flex flex-col
+                    items-center
+                    justify-start
+                    w-full
+                    hover:border-gray-500
+                  "
+                >
+                  <MilSymbol :size="32" :sidc="sidc" />
+                  <p
+                    class="
+                      mt-1
+                      text-sm text-center text-gray-900
+                      max-w-full
+                      overflow-hidden
+                    "
+                  >
+                    {{ entitySubtype || entityType || entity }}
+                  </p>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </TabItem>
+    </TabView>
   </SimpleModal>
 </template>
 
@@ -134,10 +196,14 @@ import { useSymbolItems } from "../composables/symbolData";
 import NProgress from "nprogress";
 import SearchModalInput from "./SearchModalInput.vue";
 import { GlobalEvents } from "vue-global-events";
+import TabView from "./TabView.vue";
+import TabItem from "./TabItem.vue";
 
 export default defineComponent({
   name: "SymbolPickerModal",
   components: {
+    TabItem,
+    TabView,
     SearchModalInput,
     SymbolCodeMultilineSelect,
     SimpleModal,
@@ -249,6 +315,18 @@ export default defineComponent({
       );
     });
 
+    const iconsByEntity = computed(() => {
+      const { icons } = symbolItems;
+      const mm = new Map();
+      icons.value.forEach((v) => {
+        const l = mm.get(v.entity) || [];
+        l.push(v);
+        mm.set(v.entity, l);
+      });
+
+      return mm;
+    });
+
     return {
       open,
       csidc,
@@ -265,6 +343,7 @@ export default defineComponent({
       hitsIsOpen,
       hitsRef,
       onTab,
+      iconsByEntity,
     };
   },
 });
