f9c5e327489160d1ce221a424692190072e00471
Kjell Magne Fauske <kjellmf@gmail.com>
2021-10-06 16:15:47 +0200

    feat: add button for clearing modifiers in symbol picker modal
    Closes #38

--- src/components/SymbolPickerModal.vue
+++ src/components/SymbolPickerModal.vue
@@ -1,136 +1,144 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <SimpleModal v-model="open" dialog-title="Symbol picker">
-    <header class="mt-4 w-16 h-20">
-      <MilSymbol :sidc="csidc" :size="34" />
-    </header>
+    <div class="h-full flex flex-col">
+      <header class="mt-4 w-16 h-20">
+        <MilSymbol :sidc="csidc" :size="34" />
+      </header>
 
-    <TabView>
-      <TabItem label="Select" v-slot="{ isActive }">
-        <SearchModalInput
-          class="pb-3"
-          placeholder="Search for symbol"
-          v-model="searchQuery"
-          @keydown.tab="onTab"
-          @keydown.esc.stop="hitsIsOpen ? (hitsIsOpen = false) : (open = false)"
-          @keydown.enter.prevent="!hitsIsOpen && onSubmit()"
-        />
-
-        <div class="relative" v-if="hits?.length && hitsIsOpen" ref="hitsRef">
-          <ul
-            class="
-              absolute
-              z-10
-              mt-1
-              w-full
-              bg-white
-              shadow-2xl
-              max-h-56
-              border border-gray-400
-              rounded-md
-              py-1
-              text-base
-              ring-1 ring-black ring-opacity-5
-              overflow-auto
-              focus:outline-none
-              sm:text-sm
-              divide-y divide-gray-100
+      <TabView class="flex-auto">
+        <TabItem label="Select" v-slot="{ isActive }">
+          <SearchModalInput
+            class="pb-3"
+            placeholder="Search for symbol"
+            v-model="searchQuery"
+            @keydown.tab="onTab"
+            @keydown.esc.stop="
+              hitsIsOpen ? (hitsIsOpen = false) : (open = false)
             "
-          >
-            <li
-              v-for="(item, index) in hits"
-              class="
-                text-sm
-                p-2
-                py-3
-                flex
-                items-center
-                cursor-default
-                hover:bg-gray-200
-              "
-              tabindex="-1"
-              :class="{ 'bg-gray-200': index === currentIndex }"
-              :key="item.sidc"
-              :id="item.sidc"
-              @click="onSelect(index)"
-            >
-              <p class="flex-shrink-0 h-7 w-9 flex justify-center">
-                <MilSymbol :size="25" :sidc="item.sidc" />
-              </p>
-              <span class="text-sm ml-3">{{ item.text }} </span>
-            </li>
-          </ul>
-        </div>
-        <form class="space-y-4" @submit.prevent="onSubmit" v-if="isLoaded">
-          <!--      <input type="text" :value="sidc" />-->
-          <SymbolCodeSelect
-            v-model="symbolSetValue"
-            label="Symbol set"
-            :items="symbolSets"
-          />
-          <SymbolCodeSelect
-            v-model="statusValue"
-            label="Status"
-            :items="statusItems"
-          />
-          <SymbolCodeSelect
-            v-model="hqtfdValue"
-            label="Headquaters / Task force / Dummy"
-            :items="hqtfdItems"
-          />
-          <SymbolCodeSelect
-            v-model="emtValue"
-            label="Echelon / Mobility / Towed array"
-            :items="emtItems"
-          />
-          <SymbolCodeMultilineSelect
-            v-model="iconValue"
-            label="Main icon"
-            :items="icons"
-          />
-          <SymbolCodeSelect
-            v-model="mod1Value"
-            label="Modifier 1"
-            :items="mod1Items"
-          />
-          <SymbolCodeSelect
-            v-model="mod2Value"
-            label="Modifier 2"
-            :items="mod2Items"
+            @keydown.enter.prevent="!hitsIsOpen && onSubmit()"
           />
 
-          <div style="min-height: 14rem" class="py-4 flex justify-end">
-            <div class="">
-              <PrimaryButton type="submit" class=""
-                >Select symbol
-              </PrimaryButton>
-            </div>
+          <div class="relative" v-if="hits?.length && hitsIsOpen" ref="hitsRef">
+            <ul
+              class="
+                absolute
+                z-10
+                mt-1
+                w-full
+                bg-white
+                shadow-2xl
+                max-h-56
+                border border-gray-400
+                rounded-md
+                py-1
+                text-base
+                ring-1 ring-black ring-opacity-5
+                overflow-auto
+                focus:outline-none
+                sm:text-sm
+                divide-y divide-gray-100
+              "
+            >
+              <li
+                v-for="(item, index) in hits"
+                class="
+                  text-sm
+                  p-2
+                  py-3
+                  flex
+                  items-center
+                  cursor-default
+                  hover:bg-gray-200
+                "
+                tabindex="-1"
+                :class="{ 'bg-gray-200': index === currentIndex }"
+                :key="item.sidc"
+                :id="item.sidc"
+                @click="onSelect(index)"
+              >
+                <p class="flex-shrink-0 h-7 w-9 flex justify-center">
+                  <MilSymbol :size="25" :sidc="item.sidc" />
+                </p>
+                <span class="text-sm ml-3">{{ item.text }} </span>
+              </li>
+            </ul>
           </div>
-        </form>
-        <GlobalEvents
-          v-if="isActive && hits?.length && hitsIsOpen"
-          @keydown.arrow-down="doKbd('down')"
-          @keydown.page-down="doKbd('pagedown')"
-          @keydown.arrow-up="doKbd('up')"
-          @keydown.page-up="doKbd('pageup')"
-          @keydown.enter.prevent="onSelect()"
+          <form class="space-y-4" @submit.prevent="onSubmit" v-if="isLoaded">
+            <!--      <input type="text" :value="sidc" />-->
+            <SymbolCodeSelect
+              v-model="symbolSetValue"
+              label="Symbol set"
+              :items="symbolSets"
+            />
+            <SymbolCodeSelect
+              v-model="statusValue"
+              label="Status"
+              :items="statusItems"
+            />
+            <SymbolCodeSelect
+              v-model="hqtfdValue"
+              label="Headquaters / Task force / Dummy"
+              :items="hqtfdItems"
+            />
+            <SymbolCodeSelect
+              v-model="emtValue"
+              label="Echelon / Mobility / Towed array"
+              :items="emtItems"
+            />
+            <SymbolCodeMultilineSelect
+              v-model="iconValue"
+              label="Main icon"
+              :items="icons"
+            />
+            <SymbolCodeSelect
+              v-model="mod1Value"
+              label="Modifier 1"
+              :items="mod1Items"
+            />
+            <SymbolCodeSelect
+              v-model="mod2Value"
+              label="Modifier 2"
+              :items="mod2Items"
+            />
+
+            <div style="min-height: 14rem" class="py-4 flex justify-end">
+              <div class=""></div>
+            </div>
+          </form>
+          <GlobalEvents
+            v-if="isActive && hits?.length && hitsIsOpen"
+            @keydown.arrow-down="doKbd('down')"
+            @keydown.page-down="doKbd('pagedown')"
+            @keydown.arrow-up="doKbd('up')"
+            @keydown.page-up="doKbd('pageup')"
+            @keydown.enter.prevent="onSelect()"
+          >
+          </GlobalEvents>
+        </TabItem>
+        <TabItem label="Browse" v-slot="{ isActive }">
+          <SymbolBrowseTab
+            v-if="isActive"
+            v-model:icon-value="iconValue"
+            v-model:symbol-set-value="symbolSetValue"
+            v-model:mod1-value="mod1Value"
+            v-model:mod2-value="mod2Value"
+            :symbol-sets="symbolSets"
+            :icons="icons"
+            :mod1-items="mod1Items"
+            :mod2-items="mod2Items"
+          />
+        </TabItem>
+      </TabView>
+      <div class="pt-4 flex-shrink-0 flex justify-end space-x-2">
+        <SecondaryButton type="button" @click="clearModifiers()" class=""
+          >Clear modifiers</SecondaryButton
         >
-        </GlobalEvents>
-      </TabItem>
-      <TabItem label="Browse" v-slot="{ isActive }">
-        <SymbolBrowseTab
-          v-if="isActive"
-          v-model:icon-value="iconValue"
-          v-model:symbol-set-value="symbolSetValue"
-          v-model:mod1-value="mod1Value"
-          v-model:mod2-value="mod2Value"
-          :symbol-sets="symbolSets"
-          :icons="icons"
-          :mod1-items="mod1Items"
-          :mod2-items="mod2Items"
-        />
-      </TabItem>
-    </TabView>
+        <PrimaryButton type="button" @click="onSubmit()" class=""
+          >Select symbol
+        </PrimaryButton>
+      </div>
+    </div>
   </SimpleModal>
 </template>
 
@@ -156,10 +164,12 @@ import { GlobalEvents } from "vue-global-events";
 import TabView from "./TabView.vue";
 import TabItem from "./TabItem.vue";
 import SymbolBrowseTab from "./SymbolBrowseTab.vue";
+import SecondaryButton from "./SecondaryButton.vue";
 
 export default defineComponent({
   name: "SymbolPickerModal",
   components: {
+    SecondaryButton,
     SymbolBrowseTab,
     TabItem,
     TabView,
@@ -274,6 +284,13 @@ export default defineComponent({
       );
     });
 
+    function clearModifiers() {
+      symbolItems.mod1Value.value = "00";
+      symbolItems.mod2Value.value = "00";
+      symbolItems.emtValue.value = "00";
+      symbolItems.hqtfdValue.value = "0";
+    }
+
     return {
       open,
       csidc,
@@ -290,6 +307,7 @@ export default defineComponent({
       hitsIsOpen,
       hitsRef,
       onTab,
+      clearModifiers,
     };
   },
 });
