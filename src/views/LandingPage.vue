12ed0460f865090be0ca391c561b173ca8748e5f
Kjell Magne Fauske <kjellmf@gmail.com>
2022-01-02 12:57:58 +0100

    Add temporary landing page

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,35 +1,60 @@
 <template>
-  <div class="min-h-screen bg-gray-100 overflow-hidden">
-    <main>
-      <ProseSection class="">
-        <h1><span class="text-red-900">Order of Battle</span> Mapper</h1>
-        <p>Recreate historic battles.</p>
-
-        <p>
-          Follow the development of
-          <a href="https://github.com/kjellmf/orbat-mapper"
-            >ORBAT Mapper on GitHub <GithubIcon class="w-10 h-10 inline" /> </a
-          >.
+  <div class="pb-8">
+    <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+      <div class="text-center">
+        <h1
+          class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+        >
+          <span class="block xl:inline">Order of Battle</span>
+          {{ " " }}
+          <span class="block text-red-900 xl:inline">mapper</span>
+        </h1>
+        <p
+          class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+        >
+          Recreate historic battles and military scenarios.
         </p>
-        <p>
-          <router-link to="/storymode" class="">Story mode test</router-link>
-        </p>
 
-        <div class="grid grid-cols-2 place-items-center gap-4">
-          <div class="aspect-w-5 aspect-h-2 rounded-lg bg-gray-100 overflow-hidden">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4">
+          <div class="border rounded-lg overflow-hidden">
             <img src="/images/orbat-chart-demo.png" class="object-cover object-top" />
           </div>
-          <router-link :to="{ name: ORBAT_CHART_ROUTE }" class="flex-shrink-0"
-            >Orbat chart test
-          </router-link>
+          <div class="border rounded-lg">
+            <img src="/images/screenshot-2021-08-04.png" class="object-cover" />
+          </div>
         </div>
-      </ProseSection>
-      <LandingPageScenarios />
 
-      <ProseSection class="">
-        <!--        <h2>Features</h2>-->
-        <!--        <h2>Credits</h2>-->
-      </ProseSection>
+        <p
+          class="mt-3 max-w-md mx-auto text-base text-green-900-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+        >
+          ORBAT mapper is currently under development. You can follow the
+          <a href="https://github.com/kjellmf/orbat-mapper">development on Github</a> and
+          play with a
+          <a href="https://preview---orbat-mapper.netlify.app">preview version</a>.
+        </p>
+
+        <div class="mt-5 mx-auto sm:flex sm:justify-center md:mt-8">
+          <div class="rounded-md shadow">
+            <a
+              href="https://github.com/kjellmf/orbat-mapper"
+              class="w-full flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+            >
+              View on Github
+              <GithubIcon class="ml-4 w-10 h-10 inline" />
+            </a>
+          </div>
+          <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+            <a
+              href="https://preview--orbat-mapper.netlify.app"
+              class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+            >
+              Try preview
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <ProseSection class=""></ProseSection>
 
       <ProseSection class="">
         <h2>Other tools and resources</h2>
@@ -89,18 +114,12 @@
 
 <script>
 import ProseSection from "../components/ProseSection.vue";
-import LandingPageScenarios from "./LandingPageScenarios.vue";
 import { Github as GithubIcon } from "mdue";
-import { ORBAT_CHART_ROUTE } from "../routes";
 
 export default {
   components: {
-    LandingPageScenarios,
     ProseSection,
     GithubIcon,
-  },
-  setup() {
-    return { ORBAT_CHART_ROUTE };
   },
 };
 </script>
