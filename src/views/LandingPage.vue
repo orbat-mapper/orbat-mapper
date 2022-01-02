fc6f5cf62fb7626d83a269791537c58210b2a905
Kjell Magne Fauske <kjellmf@gmail.com>
2022-01-02 16:05:17 +0100

    Tweak landing page

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -15,9 +15,11 @@
           Recreate historic battles and military scenarios.
         </p>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4">
+        <div
+          class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-6 max-w-sm md:max-w-5xl mx-auto"
+        >
           <div class="border rounded-lg overflow-hidden">
-            <img src="/images/orbat-chart-demo.png" class="object-cover object-top" />
+            <img src="/images/orbat-chart-demo.png" class="object-cover object-center" />
           </div>
           <div class="border rounded-lg">
             <img src="/images/screenshot-2021-08-04.png" class="object-cover" />
@@ -25,12 +27,14 @@
         </div>
 
         <p
-          class="mt-3 max-w-md mx-auto text-base text-green-900-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+          class="mt-4 max-w-md mx-auto text-base text-green-900-500 sm:text-lg md:mt-6 md:text-xl md:max-w-3xl"
         >
           ORBAT mapper is currently under development. You can follow the
           <a href="https://github.com/kjellmf/orbat-mapper">development on Github</a> and
-          play with a
-          <a href="https://preview---orbat-mapper.netlify.app">preview version</a>.
+
+          <a href="https://preview--orbat-mapper.netlify.app"
+            >try a work in progress preview</a
+          >.
         </p>
 
         <div class="mt-5 mx-auto sm:flex sm:justify-center md:mt-8">
@@ -40,10 +44,10 @@
               class="w-full flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
             >
               View on Github
-              <GithubIcon class="ml-4 w-10 h-10 inline" />
+              <GithubIcon class="ml-4 w-8 h-8 inline" />
             </a>
           </div>
-          <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+          <div class="mt-3 border border-gray-200 rounded-md shadow sm:mt-0 sm:ml-3">
             <a
               href="https://preview--orbat-mapper.netlify.app"
               class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
