0dde4b47cc696b04818c6f5c481e5ad13b8ef390
Kjell Magne Fauske <kjellmf@gmail.com>
2022-02-01 18:23:31 +0100

    feat: add support for drag and drop loading of scenarios

--- src/modules/scenarioeditor/LoadScenarioDialog.vue
+++ src/modules/scenarioeditor/LoadScenarioDialog.vue
@@ -1,12 +1,32 @@
 <template>
   <SimpleModal v-model="open" dialog-title="Load scenario">
-    <input type="file" id="file" @change="onFileLoad" />
-    <label for="file">Upload</label>
+    <div
+      class="mt-4 h-40 w-full border-2 border-dashed p-4 hover:border-gray-500"
+      :class="isDragOver ? 'cursor-crosshair border-green-500' : ''"
+      @dragover.prevent="isDragOver = true"
+      @drop.prevent="onDrop"
+      @dragleave="isDragOver = false"
+    >
+      <input
+        type="file"
+        id="file"
+        @change="onFileLoad"
+        class="absolute h-[0.1px] w-[0.1px] opacity-0"
+      />
+      <label
+        for="file"
+        class="flex h-full w-full cursor-pointer items-center justify-center hover:text-gray-700"
+        ><p>Drag a file here or click to select local file</p></label
+      >
+    </div>
+    <p v-if="isError" class="p-2 text-center text-base text-red-900">
+      Please select a valid scenario file.
+    </p>
   </SimpleModal>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { useVModel } from "@vueuse/core";
 import SimpleModal from "../../components/SimpleModal.vue";
 import { Scenario } from "../../types/models";
@@ -18,15 +38,17 @@ export default defineComponent({
     modelValue: { type: Boolean, default: false },
   },
   emits: ["update:modelValue", "loaded"],
-  setup(props, { emit }) {
+  setup: function (props, { emit }) {
     const open = useVModel(props, "modelValue");
-    const onFileLoad = (e: Event) => {
-      const target = <HTMLInputElement>e.target;
-      if (!target?.files?.length) return;
-      const file = target.files[0];
+    const isDragOver = ref(false);
+    const isError = ref(false);
+
+    function readFile(file: File) {
       const reader = new FileReader();
+
       reader.onload = function (evt) {
         const content = evt?.target?.result as string;
+
         try {
           const scenarioData = JSON.parse(content) as Scenario;
           if (scenarioData?.type === "ORBAT-mapper") {
@@ -35,12 +57,26 @@ export default defineComponent({
           }
         } catch (e) {
           console.error("Failed to load", file.name);
+          isError.value = true;
         }
       };
       reader.readAsText(file);
+    }
+
+    const onFileLoad = (e: Event) => {
+      const target = <HTMLInputElement>e.target;
+      if (!target?.files?.length) return;
+      const file = target.files[0];
+      if (file) readFile(file);
     };
 
-    return { open, onFileLoad };
+    function onDrop(ev: DragEvent) {
+      const file = ev?.dataTransfer?.files[0];
+      if (file) readFile(file);
+      isDragOver.value = false;
+    }
+
+    return { open, onFileLoad, isDragOver, onDrop, isError };
   },
 });
 </script>
