573efb5e27f98a7844a2df357ee78705be584cf3
Kjell Magne Fauske <kjellmf@gmail.com>
2022-03-13 10:42:17 +0100

    fix: reduce toolbar button width

--- src/components/ToolbarButton.vue
+++ src/components/ToolbarButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     type="button"
-    class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+    class="relative inline-flex items-center border border-gray-300 bg-white px-3 py-2 text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:px-2"
     :class="[start && 'rounded-l-md', end && 'rounded-r-md']"
   >
     <slot />
