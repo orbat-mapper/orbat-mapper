fac5e4b19743b45037ea8faa02c7daa7eabed0b0
Kjell Magne Fauske <kjellmf@gmail.com>
2022-03-21 20:32:48 +0100

    fix: add styling for disabled state and tweak active state styling

--- src/components/ToolbarButton.vue
+++ src/components/ToolbarButton.vue
@@ -1,13 +1,13 @@
 <template>
   <button
     type="button"
-    class="relative inline-flex items-center border border-gray-300 px-3 py-2 text-sm font-medium hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:px-2"
+    class="relative inline-flex items-center border border-gray-300 px-3 py-2 text-sm font-medium hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 disabled:text-gray-300 sm:px-2"
     :class="[
       start && 'rounded-l-md',
       end && 'rounded-r-md',
       top && 'rounded-t-md',
       bottom && 'rounded-b-md',
-      active ? 'bg-gray-100 text-gray-900' : 'bg-white text-gray-500',
+      active ? 'bg-gray-100 text-black' : 'bg-white text-gray-500',
     ]"
   >
     <slot />
