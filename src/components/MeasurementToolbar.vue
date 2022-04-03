4572328086db537b8b731f6d1801ecd8396011e7
Kjell Magne Fauske <kjellmf@gmail.com>
2022-04-03 13:16:58 +0200

    feat: cancel measurements on 'Escape'

--- src/components/MeasurementToolbar.vue
+++ src/components/MeasurementToolbar.vue
@@ -59,7 +59,7 @@ import ToolbarButton from "./ToolbarButton.vue";
 import OLMap from "ol/Map";
 import { useMeasurementInteraction } from "../composables/geoMeasurement";
 import { watch } from "vue";
-import { useToggle } from "@vueuse/core";
+import { onKeyStroke, useToggle } from "@vueuse/core";
 import { storeToRefs } from "pinia";
 import { useMeasurementsStore } from "../stores/geoStore";
 
@@ -77,4 +77,6 @@ const { clear } = useMeasurementInteraction(props.olMap, measurementType, {
 });
 
 watch(enableMeasurements, (v) => !v && clear());
+
+onKeyStroke("Escape", () => (enableMeasurements.value = false));
 </script>
