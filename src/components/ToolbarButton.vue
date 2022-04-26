92a40cd058cc0b4da816cfce5260b08c0735fee1
Kjell Magne Fauske <kjellmf@gmail.com>
2022-04-26 22:19:21 +0200

    refactor: tweak active button style

--- src/components/ToolbarButton.vue
+++ src/components/ToolbarButton.vue
@@ -1,13 +1,15 @@
 <template>
   <button
     type="button"
-    class="relative inline-flex items-center border border-gray-300 px-3 py-2 text-sm font-medium hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 disabled:text-gray-300 sm:px-2"
+    class="relative inline-flex items-center border border-gray-300 px-3 py-2 text-sm font-medium focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 disabled:text-gray-300 sm:px-2"
     :class="[
       start && 'rounded-l-md',
       end && 'rounded-r-md',
       top && 'rounded-t-md',
       bottom && 'rounded-b-md',
-      active ? 'bg-gray-100 text-black' : 'bg-white text-gray-500',
+      active
+        ? 'bg-army2 text-gray-200 hover:text-gray-300'
+        : 'bg-white text-gray-500 hover:bg-gray-50',
     ]"
   >
     <slot />
