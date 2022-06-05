ae8f3a1a022feac6490746a4d725ceb0d0e6400a
Kjell Magne Fauske <kjellmf@gmail.com>
2022-06-05 20:17:32 +0200

    fix: disable global dark mode detection for now
    Enable this when dark mode is usable

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { useDark } from "@vueuse/core";
-
-const isDark = useDark();
+// import { useDark } from "@vueuse/core";
+//
+// const isDark = useDark();
 </script>
