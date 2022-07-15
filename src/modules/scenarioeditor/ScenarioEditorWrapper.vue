cc54e67fecaf12cf46e6113fece195a6a3b8dfcc
Kjell Magne Fauske <kjellmf@gmail.com>
2022-07-15 08:58:46 +0200

    fix: reset load query parameter

--- src/modules/scenarioeditor/ScenarioEditorWrapper.vue
+++ src/modules/scenarioeditor/ScenarioEditorWrapper.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import ScenarioEditor from "@/modules/scenarioeditor/ScenarioEditor.vue";
 import { useScenario } from "@/scenariostore";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { watch } from "vue";
 
 const { scenario, isLoading, isReady } = useScenario();
 const route = useRoute();
+const router = useRouter();
+let demoLoaded = false;
 
 if (route.query.load) {
   scenario.value.io.loadDemoScenario(route.query.load as string);
+  demoLoaded = true;
 } else {
   if (!isReady.value) scenario.value.io.loadEmptyScenario();
 }
@@ -16,9 +19,20 @@ if (route.query.load) {
 watch(
   () => route.query.load,
   async (v) => {
-    if (v) await scenario.value.io.loadDemoScenario(v as string);
+    if (v) {
+      demoLoaded = true;
+      await scenario.value.io.loadDemoScenario(v as string);
+    }
   }
 );
+
+watch(scenario, (v) => {
+  if (!demoLoaded && route.query.load) {
+    router.replace({ query: {} });
+  } else {
+    demoLoaded = false;
+  }
+});
 </script>
 <template>
   <ScenarioEditor
