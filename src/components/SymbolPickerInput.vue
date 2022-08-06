5c1c9358f36cdbc8e46d50e04de4f0acd2075d6b
Kjell Magne Fauske <kjellmf@gmail.com>
2022-08-06 09:07:47 +0200

    refactor: convert component to script setup

--- src/components/SymbolPickerInput.vue
+++ src/components/SymbolPickerInput.vue
@@ -12,7 +12,7 @@
             name="sidc"
             id="sid"
             class="block w-full rounded-none rounded-l-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-            placeholder="John Doe"
+            placeholder="10031000001211000000"
           />
         </div>
         <button
@@ -33,37 +33,26 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineAsyncComponent, defineComponent, ref } from "vue";
+<script setup lang="ts">
+import { defineAsyncComponent, ref } from "vue";
 import { useVModel } from "@vueuse/core";
-import InputGroup from "./InputGroup.vue";
-import PlainButton from "./PlainButton.vue";
-import { biSyncDelayedRef } from "../composables/utils";
+import { biSyncDelayedRef } from "@/composables/utils";
 import { MenuAlt3Icon } from "@heroicons/vue/solid";
 import NProgress from "nprogress";
 
-export default defineComponent({
-  name: "SymbolPickerInput",
-  components: {
-    PlainButton,
-    InputGroup,
-    MenuAlt3Icon,
-    SymbolPickerModal: defineAsyncComponent(() => import("./SymbolPickerModal.vue")),
-  },
-  props: { modelValue: { type: String } },
-  emits: ["update:modelValue"],
-  setup(props) {
-    const sidcValue = useVModel(props, "modelValue");
-    const showSymbolPicker = ref(false);
-    const showSymbolPickerDelayed = ref(false);
+const SymbolPickerModal = defineAsyncComponent(() => import("./SymbolPickerModal.vue"));
 
-    biSyncDelayedRef(showSymbolPicker, showSymbolPickerDelayed);
+const props = defineProps<{ modelValue?: string }>();
+const emit = defineEmits(["update:modelValue"]);
 
-    const openModal = async () => {
-      NProgress.start();
-      showSymbolPicker.value = true;
-    };
-    return { sidcValue, showSymbolPicker, openModal, showSymbolPickerDelayed };
-  },
-});
+const sidcValue = useVModel(props, "modelValue", emit);
+const showSymbolPicker = ref(false);
+const showSymbolPickerDelayed = ref(false);
+
+biSyncDelayedRef(showSymbolPicker, showSymbolPickerDelayed);
+
+const openModal = async () => {
+  NProgress.start();
+  showSymbolPicker.value = true;
+};
 </script>
