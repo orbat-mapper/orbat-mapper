b376c2231489d49f27226022e3b5e3cf92d67d17
Kjell Magne Fauske <kjellmf@gmail.com>
2022-09-20 19:55:46 +0200

    refactor: convert DotsMenu component to floating ui

--- src/components/DotsMenu.vue
+++ src/components/DotsMenu.vue
@@ -1,22 +1,16 @@
 <template>
   <Menu as="div">
-    <MenuButton
-      @click.stop=""
-      class="inline-flex h-8 w-8 items-center justify-center rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-army2 focus:ring-offset-2"
-    >
-      <span class="sr-only">Open options</span>
-      <EllipsisVerticalIcon class="h-5 w-5" aria-hidden="true" />
-    </MenuButton>
-    <transition
-      enter-active-class="transition ease-out duration-100"
-      enter-from-class="transform opacity-0 scale-95"
-      enter-to-class="transform opacity-100 scale-100"
-      leave-active-class="transition ease-in duration-75"
-      leave-from-class="transform opacity-100 scale-100"
-      leave-to-class="transform opacity-0 scale-95"
-    >
+    <Float placement="left-start" portal flip shift>
+      <MenuButton
+        @click.stop=""
+        class="inline-flex h-8 w-8 items-center justify-center rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-army2 focus:ring-offset-2"
+      >
+        <span class="sr-only">Open options</span>
+        <EllipsisVerticalIcon class="h-5 w-5" aria-hidden="true" />
+      </MenuButton>
+
       <MenuItems
-        class="absolute right-10 top-3 z-20 mx-3 mt-1 w-48 origin-top-right divide-y divide-gray-200 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800"
+        class="mx-3 mt-1 w-48 divide-y divide-gray-200 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800"
       >
         <div class="py-1">
           <MenuItem
@@ -39,7 +33,7 @@
           </MenuItem>
         </div>
       </MenuItems>
-    </transition>
+    </Float>
   </Menu>
 </template>
 
@@ -47,6 +41,7 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
 import { EllipsisVerticalIcon } from "@heroicons/vue/20/solid";
 import { MenuItemData } from "@/components/types";
+import { Float } from "@headlessui-float/vue";
 
 const props = defineProps<{ items: MenuItemData[] }>();
 const emit = defineEmits(["action"]);
