1e5e47193b1995ab9d1f01b6caf1ac5db796f743
Kjell Magne Fauske <kjellmf@gmail.com>
2022-10-05 17:53:44 +0200

    fix: move app notifications so that they don't hide the navbar

--- src/components/AppNotifications.vue
+++ src/components/AppNotifications.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     aria-live="assertive"
-    class="pointer-events-none fixed inset-0 z-50 flex items-end px-4 py-6 sm:items-start sm:p-6"
+    class="pointer-events-none fixed inset-0 z-50 flex items-end px-2 py-6 sm:items-start sm:py-16"
   >
     <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
       <NotificationItem
