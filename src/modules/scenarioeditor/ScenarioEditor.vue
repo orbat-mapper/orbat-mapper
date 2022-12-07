02d2e85015bdd0c3fb15125edca72ad1d885a8f5
Kjell Magne Fauske <kjellmf@gmail.com>
2022-12-07 19:22:32 +0100

    fix: typo

--- src/modules/scenarioeditor/ScenarioEditor.vue
+++ src/modules/scenarioeditor/ScenarioEditor.vue
@@ -27,9 +27,9 @@
       </div>
       <div class="flex shrink-0 items-center space-x-2">
         <a
-          href=https://docs.orbat-mapper.app/guide/what-is-orbat-mapper
+          href="https://docs.orbat-mapper.app/guide/what-is-orbat-mapper"
           target="_blank"
-          class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white font-medium"
+          class="inline-flex items-center justify-center rounded-md p-2 font-medium text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
         >
           Help
         </a>
