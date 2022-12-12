7b37c8e6cb5a97d89992f43c75269820c7907535
Kjell Magne Fauske <kjellmf@gmail.com>
2022-12-12 19:18:10 +0100

    feat: start experimenting with chart edit mode

--- src/modules/scenarioeditor/ChartEditView.vue
+++ src/modules/scenarioeditor/ChartEditView.vue
@@ -1,7 +1,57 @@
 <template>
-  <div class="flex h-full w-full items-center justify-center">
-    <p class="text-2xl font-medium text-gray-700">Not implemented yet.</p>
+  <div class="relative flex min-h-0 flex-auto">
+    <aside
+      class="relative z-10 flex flex-col justify-between overflow-auto border-r-2 bg-gray-100 dark:bg-gray-900"
+    >
+      <OrbatPanel class="w-96 space-y-1">
+        <template #header></template>
+      </OrbatPanel>
+    </aside>
+    <main class="relative min-w-0 flex-auto">
+      <OrbatChart
+        :unit="activeUnit"
+        :width="width"
+        :height="height"
+        :symbol-generator="symbolGenerator"
+        chart-id="chartId"
+        :options="options.$state"
+        :specific-options="specificOptions.$state"
+      />
+    </main>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+import OrbatPanel from "@/modules/scenarioeditor/OrbatPanel.vue";
+import { symbolGenerator } from "@/symbology/milsymbwrapper";
+import {
+  useChartSettingsStore,
+  useRootUnitStore,
+  useSpecificChartOptionsStore,
+} from "@/modules/charteditor/chartSettingsStore";
+import { sizeToWidthHeight } from "@/modules/charteditor/orbatchart/sizes";
+import OrbatChart from "@/modules/charteditor/OrbatChart.vue";
+import { injectStrict } from "@/utils";
+import { activeScenarioKey, activeUnitKey } from "@/components/injects";
+
+const rootUnitStore = useRootUnitStore();
+const options = useChartSettingsStore();
+const specificOptions = useSpecificChartOptionsStore();
+const activeUnitId = injectStrict(activeUnitKey);
+const {
+  unitActions,
+  store: { state },
+} = injectStrict(activeScenarioKey);
+const activeUnit = computed(
+  () =>
+    (activeUnitId.value &&
+      unitActions.expandUnit(state.getUnitById(activeUnitId.value))) ||
+    null
+);
+
+rootUnitStore.unit = null;
+
+const width = computed(() => sizeToWidthHeight(options.paperSize).width);
+const height = computed(() => sizeToWidthHeight(options.paperSize).height);
+</script>
