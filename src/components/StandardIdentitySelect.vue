f5c9c1050ad1a0b942a27b53212eec54102c65c7
Kjell Magne Fauske <kjellmf@gmail.com>
2023-03-25 20:30:53 +0100

    fix: add civilian color

--- src/components/StandardIdentitySelect.vue
+++ src/components/StandardIdentitySelect.vue
@@ -151,6 +151,7 @@ const colors: Omit<SymbolItem, "sidc">[] = [
   { code: "#ff8080", text: "Red (standard)" },
   { code: "#aaffaa", text: "Green (standard)" },
   { code: "#ffff80", text: "Yellow (standard)" },
+  { code: "#ffa1ff", text: "Pink (civilian)" },
   { code: "#aab074", text: "Olive" },
   { code: "#5baa5b", text: "Infantry (Battle Order)" },
   { code: "#ffd00b", text: "Armor (Battle Order)" },
