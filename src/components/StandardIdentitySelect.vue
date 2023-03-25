c6ec7b2eb3eb9286449b31c6002979f1b9285fe0
Kjell Magne Fauske <kjellmf@gmail.com>
2023-03-25 16:25:49 +0100

    feat: add more fill colors

--- src/components/StandardIdentitySelect.vue
+++ src/components/StandardIdentitySelect.vue
@@ -147,11 +147,17 @@ const items = computed(() =>
 
 const colors: Omit<SymbolItem, "sidc">[] = [
   { code: "", text: "Standard" },
-  { code: "#0000ff", text: "Blue" },
-  { code: "#00ff00", text: "Green" },
-  { code: "#ff0000", text: "Red" },
-  { code: "#ffff00", text: "Yellow" },
-  { code: "#ffffff", text: "White" },
+  { code: "#80e0ff", text: "Blue (standard)" },
+  { code: "#ff8080", text: "Red (standard)" },
+  { code: "#aaffaa", text: "Green (standard)" },
+  { code: "#ffff80", text: "Yellow (standard)" },
+  { code: "#aab074", text: "Olive" },
+  { code: "#5baa5b", text: "Infantry (Battle Order)" },
+  { code: "#ffd00b", text: "Armor (Battle Order)" },
+  { code: "#ff3333", text: "Artillery (Battle Order)" },
+  { code: "#f7f7f7", text: "Combat Support (Battle Order)" },
+  { code: "#d87600", text: "Service Support (Battle Order)" },
+  { code: "#a2e3e8", text: "Aviation (Battle Order)" },
 ];
 
 const colorIconItems = computed((): SymbolItem[] =>
