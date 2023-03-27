6751fcc6a152daeae03a971e9a6da4706a877bb9
Kjell Magne Fauske <kjellmf@gmail.com>
2023-03-27 19:14:01 +0200

    fix: revert use of portal option for symbol selects

--- src/components/SymbolCodeMultilineSelect.vue
+++ src/components/SymbolCodeMultilineSelect.vue
@@ -4,95 +4,96 @@
     <ListboxLabel class="block text-sm font-medium text-gray-700">
       {{ label }}
     </ListboxLabel>
-    <div class="relative mt-1">
-      <Float placement="bottom" flip portal adaptive-width :offset="4">
-        <ListboxButton
-          class="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
-        >
-          <span class="flex items-center">
-            <MilitarySymbol
-              :sidc="selected?.sidc || ''"
-              alt=""
-              :size="20"
-              class="h-8 w-10 flex-shrink-0 pt-0.5"
-              :options="symbolOptions"
-            />
-            <div class="ml-3">
-              <p v-if="selected?.subLabel" class="text-xs text-gray-600">
-                {{ selected.subLabel }}
-              </p>
+    <Float
+      as="div"
+      floating-as="template"
+      class="relative mt-1"
+      placement="bottom"
+      flip
+      :offset="4"
+    >
+      <ListboxButton
+        class="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
+      >
+        <span class="flex items-center">
+          <MilitarySymbol
+            :sidc="selected?.sidc || ''"
+            alt=""
+            :size="20"
+            class="h-8 w-10 flex-shrink-0 pt-0.5"
+            :options="symbolOptions"
+          />
+          <div class="ml-3">
+            <p v-if="selected?.subLabel" class="text-xs text-gray-600">
+              {{ selected.subLabel }}
+            </p>
 
-              <p class="mt-0 text-sm">
-                {{ selected?.label }}
-              </p>
-            </div>
-          </span>
-          <span
-            class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2"
-          >
-            <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-          </span>
-        </ListboxButton>
-
-        <transition
-          leave-active-class="transition ease-in duration-100"
-          leave-from-class="opacity-100"
-          leave-to-class="opacity-0"
+            <p class="mt-0 text-sm">
+              {{ selected?.label }}
+            </p>
+          </div>
+        </span>
+        <span
+          class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2"
         >
-          <ListboxOptions
-            class="max-h-56 w-full overflow-auto rounded-md border border-gray-200 bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+          <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+        </span>
+      </ListboxButton>
+
+      <transition
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <ListboxOptions
+          class="max-h-56 w-full overflow-auto rounded-md border border-gray-200 bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+        >
+          <ListboxOption
+            as="template"
+            v-for="item in renderedItems"
+            :key="item.sidc"
+            :value="item.code"
+            v-slot="{ active, selected }"
           >
-            <ListboxOption
-              as="template"
-              v-for="item in renderedItems"
-              :key="item.sidc"
-              :value="item.code"
-              v-slot="{ active, selected }"
+            <li
+              :class="[
+                active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                'relative cursor-default select-none py-2 pl-3 pr-9',
+              ]"
             >
-              <li
+              <div class="flex items-center">
+                <p class="flex h-7 w-8 flex-shrink-0 justify-center pt-1">
+                  <MilitarySymbol :size="20" :sidc="item.sidc" :options="symbolOptions" />
+                </p>
+                <div :class="[selected ? 'font-semibold' : 'font-normal', 'ml-3 ']">
+                  <p
+                    v-if="item.subLabel"
+                    class="text-xs text-gray-600"
+                    :class="{ 'text-white': active }"
+                  >
+                    {{ item.subLabel }}
+                  </p>
+
+                  <p class="mt-0 text-sm">
+                    {{ item.label }}
+                  </p>
+                </div>
+              </div>
+
+              <span
+                v-if="selected"
                 :class="[
-                  active ? 'bg-indigo-600 text-white' : 'text-gray-900',
-                  'relative cursor-default select-none py-2 pl-3 pr-9',
+                  active ? 'text-white' : 'text-indigo-600',
+                  'absolute inset-y-0 right-0 flex items-center pr-4',
                 ]"
               >
-                <div class="flex items-center">
-                  <p class="flex h-7 w-8 flex-shrink-0 justify-center pt-1">
-                    <MilitarySymbol
-                      :size="20"
-                      :sidc="item.sidc"
-                      :options="symbolOptions"
-                    />
-                  </p>
-                  <div :class="[selected ? 'font-semibold' : 'font-normal', 'ml-3 ']">
-                    <p
-                      v-if="item.subLabel"
-                      class="text-xs text-gray-600"
-                      :class="{ 'text-white': active }"
-                    >
-                      {{ item.subLabel }}
-                    </p>
-
-                    <p class="mt-0 text-sm">
-                      {{ item.label }}
-                    </p>
-                  </div>
-                </div>
-
-                <span
-                  v-if="selected"
-                  :class="[
-                    active ? 'text-white' : 'text-indigo-600',
-                    'absolute inset-y-0 right-0 flex items-center pr-4',
-                  ]"
-                >
-                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
-                </span>
-              </li>
-            </ListboxOption>
-          </ListboxOptions>
-        </transition>
-      </Float>
-    </div>
+                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+              </span>
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
+    </Float>
   </Listbox>
 </template>
 
--- src/components/SymbolCodeSelect.vue
+++ src/components/SymbolCodeSelect.vue
@@ -3,83 +3,88 @@
     <ListboxLabel class="block text-sm font-medium text-gray-700">
       {{ label }}
     </ListboxLabel>
-    <div class="relative mt-1">
-      <Float placement="bottom" flip portal adaptive-width :offset="4">
-        <ListboxButton
-          class="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
-        >
-          <div class="flex items-center">
-            <div class="flex h-8 w-10 flex-shrink-0 items-center">
-              <MilitarySymbol
-                :sidc="selected?.sidc || ''"
-                alt=""
-                :size="20"
-                :options="{ ...symbolOptions, ...selected?.symbolOptions }"
-              />
-            </div>
-            <span class="ml-3 block truncate">{{ selected?.text }}</span>
+    <Float
+      as="div"
+      floating-as="template"
+      class="relative mt-1"
+      placement="bottom"
+      flip
+      :offset="4"
+    >
+      <ListboxButton
+        class="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
+      >
+        <div class="flex items-center">
+          <div class="flex h-8 w-10 flex-shrink-0 items-center">
+            <MilitarySymbol
+              :sidc="selected?.sidc || ''"
+              alt=""
+              :size="20"
+              :options="{ ...symbolOptions, ...selected?.symbolOptions }"
+            />
           </div>
-          <span
-            class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2"
-          >
-            <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-          </span>
-        </ListboxButton>
-
-        <transition
-          leave-active-class="transition ease-in duration-100"
-          leave-from-class="opacity-100"
-          leave-to-class="opacity-0"
+          <span class="ml-3 block truncate">{{ selected?.text }}</span>
+        </div>
+        <span
+          class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2"
         >
-          <ListboxOptions
-            class="max-h-56 overflow-auto rounded-md border border-gray-200 bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-          >
-            <ListboxOption
-              as="template"
-              v-for="item in items"
-              :key="item.sidc"
-              :value="item.code"
-              v-slot="{ active, selected }"
-            >
-              <li
-                :class="[
-                  active ? 'bg-indigo-600 text-white' : 'text-gray-900',
-                  'relative cursor-default select-none py-2 pl-3 pr-9',
-                ]"
-              >
-                <div class="flex items-center">
-                  <p class="flex h-7 w-8 flex-shrink-0 items-center justify-center">
-                    <MilitarySymbol
-                      :size="20"
-                      :sidc="item.sidc"
-                      :options="{ ...symbolOptions, ...item.symbolOptions }"
-                    />
-                  </p>
-                  <span
-                    :class="[
-                      selected ? 'font-semibold' : 'font-normal',
-                      'ml-3 block truncate',
-                    ]"
-                  >
-                    {{ item.text }}
-                  </span>
-                </div>
+          <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+        </span>
+      </ListboxButton>
 
+      <transition
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <ListboxOptions
+          class="max-h-56 w-full overflow-auto rounded-md border border-gray-200 bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+        >
+          <ListboxOption
+            as="template"
+            v-for="item in items"
+            :key="item.sidc"
+            :value="item.code"
+            v-slot="{ active, selected }"
+          >
+            <li
+              :class="[
+                active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                'relative cursor-default select-none py-2 pl-3 pr-9',
+              ]"
+            >
+              <div class="flex items-center">
+                <p class="flex h-7 w-8 flex-shrink-0 items-center justify-center">
+                  <MilitarySymbol
+                    :size="20"
+                    :sidc="item.sidc"
+                    :options="{ ...symbolOptions, ...item.symbolOptions }"
+                  />
+                </p>
                 <span
-                  v-if="selected"
                   :class="[
-                    active ? 'text-white' : 'text-indigo-600',
-                    'absolute inset-y-0 right-0 flex items-center pr-4',
+                    selected ? 'font-semibold' : 'font-normal',
+                    'ml-3 block truncate',
                   ]"
                 >
-                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                  {{ item.text }}
                 </span>
-              </li>
-            </ListboxOption>
-          </ListboxOptions>
-        </transition>
-      </Float>
-    </div>
+              </div>
+
+              <span
+                v-if="selected"
+                :class="[
+                  active ? 'text-white' : 'text-indigo-600',
+                  'absolute inset-y-0 right-0 flex items-center pr-4',
+                ]"
+              >
+                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+              </span>
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
+    </Float>
   </Listbox>
 </template>
 
