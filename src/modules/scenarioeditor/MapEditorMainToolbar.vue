680baeffbd86832571f32a06e814fa44f8ef6698
Kjell Magne Fauske <kjellmf@gmail.com>
2023-04-07 14:17:55 +0200

    feat: add undo redo buttons to toolbar

--- src/modules/scenarioeditor/MapEditorMainToolbar.vue
+++ src/modules/scenarioeditor/MapEditorMainToolbar.vue
@@ -1,24 +1,34 @@
 <template>
   <FloatingPanel
-    class="pointer-events-auto flex w-full max-w-lg items-center -space-x-1 rounded-md bg-white p-1"
+    class="pointer-events-auto flex w-full max-w-lg items-center justify-between rounded-md bg-white p-1"
   >
-    <MainToolbarButton>
-      <SelectIcon class="h-6 w-6" />
-    </MainToolbarButton>
-    <MainToolbarButton
-      :active="store.currentToolbar === 'measurements'"
-      @click="store.toggleToolbar('measurements')"
-      title="Measurements"
-    >
-      <MeasurementIcon class="h-6 w-6" />
-    </MainToolbarButton>
-    <MainToolbarButton
-      :active="store.currentToolbar === 'draw'"
-      @click="store.toggleToolbar('draw')"
-      title="Draw"
-    >
-      <DrawIcon class="h-6 w-6" />
-    </MainToolbarButton>
+    <div class="flex items-center justify-between -space-x-1">
+      <MainToolbarButton>
+        <SelectIcon class="h-6 w-6" />
+      </MainToolbarButton>
+      <MainToolbarButton
+        :active="store.currentToolbar === 'measurements'"
+        @click="store.toggleToolbar('measurements')"
+        title="Measurements"
+      >
+        <MeasurementIcon class="h-6 w-6" />
+      </MainToolbarButton>
+      <MainToolbarButton
+        :active="store.currentToolbar === 'draw'"
+        @click="store.toggleToolbar('draw')"
+        title="Draw"
+      >
+        <DrawIcon class="h-6 w-6" />
+      </MainToolbarButton>
+    </div>
+    <div class="flex items-center -space-x-1">
+      <MainToolbarButton title="Undo" @click="undo()" :disabled="!canUndo">
+        <UndoIcon class="h-6 w-6" />
+      </MainToolbarButton>
+      <MainToolbarButton title="Redo" @click="redo()" :disabled="!canRedo">
+        <RedoIcon class="h-6 w-6" />
+      </MainToolbarButton>
+    </div>
   </FloatingPanel>
 </template>
 <script setup lang="ts">
@@ -26,10 +36,17 @@ import {
   IconCursorDefaultOutline as SelectIcon,
   IconPencil as DrawIcon,
   IconRulerSquareCompass as MeasurementIcon,
+  IconRedoVariant as RedoIcon,
+  IconUndoVariant as UndoIcon,
 } from "@iconify-prerendered/vue-mdi";
 import FloatingPanel from "@/components/FloatingPanel.vue";
 import MainToolbarButton from "@/components/MainToolbarButton.vue";
 import { useMainToolbarStore } from "@/stores/mainToolbarStore";
+import { injectStrict } from "@/utils";
+import { activeScenarioKey } from "@/components/injects";
 
+const {
+  store: { undo, redo, canRedo, canUndo },
+} = injectStrict(activeScenarioKey);
 const store = useMainToolbarStore();
 </script>
