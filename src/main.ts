be9409c8b5d8cf70eab7edd718fe22bb800d0e8c
Kjell Magne Fauske <kjellmf@gmail.com>
2023-04-08 11:19:08 +0200

    fix: add mobile drag and drop polyfill
    Needed for Firefox on Android. Closes #143.

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,9 @@ import "./styles.css";
 import App from "./App.vue";
 import { router } from "./router";
 import "@imengyu/vue3-context-menu/lib/vue3-context-menu.css";
+import { polyfill as dragDropPolyfill } from "mobile-drag-drop";
+
+// needed for some mobile devices (Firefox android)
+dragDropPolyfill();
 
 createApp(App).use(router).use(createPinia()).mount("#app");
