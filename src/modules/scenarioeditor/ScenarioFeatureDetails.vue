5498e358299d302d0bfb09251aabb49e19f02451
Kjell Magne Fauske <kjellmf@gmail.com>
2023-04-29 22:25:20 +0200

    feat: add experimental inline editing

--- src/modules/scenarioeditor/ScenarioFeatureDetails.vue
+++ src/modules/scenarioeditor/ScenarioFeatureDetails.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import type { SelectedScenarioFeatures } from "@/stores/dragStore";
 import { injectStrict } from "@/utils";
-import { activeScenarioKey } from "@/components/injects";
-import { computed } from "vue";
+import { activeMapKey, activeScenarioKey } from "@/components/injects";
+import { computed, ref, watch } from "vue";
+import { useScenarioLayers } from "@/modules/scenarioeditor/scenarioLayers2";
 
 interface Props {
   selectedIds: SelectedScenarioFeatures;
@@ -10,6 +11,8 @@ interface Props {
 const props = defineProps<Props>();
 
 const { geo } = injectStrict(activeScenarioKey);
+const olMapRef = injectStrict(activeMapKey);
+const { updateFeature } = useScenarioLayers(olMapRef.value);
 const feature = computed(() => {
   if (props.selectedIds.size === 1) {
     return geo.getFeatureById(props.selectedIds.values().next().value).feature;
@@ -17,13 +20,38 @@ const feature = computed(() => {
   return null;
 });
 
+const featureName = ref("DD");
+
+watch(
+  () => feature.value?.properties.name,
+  (v) => {
+    featureName.value = v ?? "";
+  },
+  { immediate: true }
+);
+
 const geometryType = computed(() => feature.value?.properties.type);
 const isMultipleFeatures = computed(() => props.selectedIds.size > 1);
+function onKey(e: KeyboardEvent) {
+  (e.target as HTMLInputElement).blur();
+}
+
+function updateValue() {
+  feature.value && updateFeature(feature.value?.id, { name: featureName.value });
+}
 </script>
 <template>
-  <section>
-    <div v-if="feature" class="prose prose-sm">
-      <h3>{{ feature.properties.name }}</h3>
+  <section class="">
+    <div v-if="feature" class="">
+      <input
+        type="text"
+        v-model="featureName"
+        @keyup.esc="onKey"
+        @keyup.enter="onKey"
+        @blur="updateValue()"
+        class="-mx-3 rounded-md border-0 text-base font-semibold leading-6 text-gray-900 ring-0 ring-inset hover:ring-1 focus:ring-2 focus:ring-inset"
+      />
+
       <p class="whitespace-pre-wrap">{{ feature.properties.description }}</p>
     </div>
   </section>
