1ab9824b2972d21bca359ce88b26a5ccdf0a5a34
Kjell Magne Fauske <kjellmf@gmail.com>
2023-07-11 19:29:58 +0200

    fix: disable spellcheck when not focused

--- src/components/EditableLabel.vue
+++ src/components/EditableLabel.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useTextareaAutosize, useVModel } from "@vueuse/core";
+import { ref } from "vue";
 
 const props = defineProps<{
   modelValue: string;
@@ -10,8 +11,19 @@ const emit = defineEmits(["update:modelValue", "update-value"]);
 const value = useVModel(props, "modelValue", emit);
 const { textarea, input } = useTextareaAutosize({ input: value });
 
+const spellcheck = ref(false);
+
 function onKey(e: KeyboardEvent) {
   (e.target as HTMLInputElement).blur();
+}
+
+function onFocus() {
+  spellcheck.value = true;
+}
+
+function onBlur() {
+  emit("update-value", value.value);
+  spellcheck.value = false;
 }
 </script>
 
@@ -22,7 +34,9 @@ function onKey(e: KeyboardEvent) {
     v-model="input"
     @keyup.esc="onKey"
     @keydown.enter.prevent="onKey"
-    @blur="emit('update-value', value)"
+    @focus="onFocus()"
+    @blur="onBlur()"
+    :spellcheck="spellcheck"
     class="-mx-3 w-full resize-none rounded-md border-0 text-base font-semibold leading-6 text-gray-900 ring-0 ring-inset hover:ring-1 focus:ring-2 focus:ring-inset"
   />
 </template>
