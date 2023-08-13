73f77186463089985262c2cf42eaaa3875c79f77
Kjell Magne Fauske <kjellmf@gmail.com>
2023-08-13 08:25:16 +0200

    fix: use plain select for TimezoneSelect component
    The combo box component from Headless UI is too slow for large lists.

--- src/components/TimezoneSelect.vue
+++ src/components/TimezoneSelect.vue
@@ -1,24 +1,14 @@
 <template>
-  <SimpleCombo :values="timeZonesNames" v-model="tzValue" />
+  <SimpleSelect :values="timeZonesNames" v-model="tzValue" />
 </template>
 
-<script>
+<script setup lang="ts">
 import { timeZonesNames } from "@vvo/tzdb";
 import { useVModel } from "@vueuse/core";
 import SimpleSelect from "./SimpleSelect.vue";
-import SimpleCombo from "./SimpleCombo.vue";
 
-export default {
-  name: "TimezoneSelect",
-  components: { SimpleCombo, SimpleSelect },
-  emits: ["update:modelValue"],
-  props: { modelValue: String },
-  setup(props) {
-    const tzValue = useVModel(props, "modelValue");
-    return {
-      timeZonesNames,
-      tzValue,
-    };
-  },
-};
+const props = defineProps<{ modelValue: string }>();
+const emit = defineEmits(["update:modelValue"]);
+
+const tzValue = useVModel(props, "modelValue", emit);
 </script>
