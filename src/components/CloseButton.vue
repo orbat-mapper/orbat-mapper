a23c15b32ca06dd63e9b1c64f8f33350767db6ed
Kjell Magne Fauske <kjellmf@gmail.com>
2023-09-07 14:12:58 +0200

    refactor: remove details panel header

--- src/components/CloseButton.vue
+++ src/components/CloseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+    class="rounded-full bg-gray-50 bg-opacity-25 text-gray-500 hover:bg-opacity-50 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
   >
     <span class="sr-only">Close</span>
     <XMarkIcon class="h-6 w-6" aria-hidden="true" />
