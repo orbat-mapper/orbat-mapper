ea8be5f47cd16450a7579f1536ba2a598145e1d0
Kjell Magne Fauske <kjellmf@gmail.com>
2023-09-09 11:42:12 +0200

    fix: update max zoom for open topo map layer

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -58,7 +58,7 @@ function createBaseLayers(view: View) {
   const openTopoMap = new TileLayer({
     source: new XYZ({
       url: "https://a.tile.opentopomap.org/{z}/{x}/{y}.png",
-      maxZoom: 17,
+      maxZoom: 14,
       attributions:
         'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
     }),
