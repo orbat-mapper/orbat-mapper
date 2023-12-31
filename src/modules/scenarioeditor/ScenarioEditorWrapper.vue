d3b9b58cc73b1a273be5c1a629c8df88e1564a67
Kjell Magne Fauske <kjellmf@gmail.com>
2023-12-31 16:56:46 +0100

    fix: ask for saving changes to demo scenario

--- src/modules/scenarioeditor/ScenarioEditorWrapper.vue
+++ src/modules/scenarioeditor/ScenarioEditorWrapper.vue
@@ -50,7 +50,7 @@ function isDemoScenario(scenarioId: string) {
 }
 
 onBeforeRouteLeave(async (to, from) => {
-  await saveScenarioIfNecessary();
+  await saveScenarioIfNecessary({ saveDemo: true });
 });
 
 useEventListener(document, "visibilitychange", async () => {
@@ -61,8 +61,21 @@ useEventListener(window, "beforeunload", async () => {
   await saveScenarioIfNecessary();
 });
 
-async function saveScenarioIfNecessary() {
+async function saveScenarioIfNecessary({ saveDemo = false } = {}) {
   if (scenario.value?.store?.canUndo?.value) {
+    if (isDemoScenario(props.scenarioId)) {
+      if (!saveDemo) {
+        return;
+      }
+      if (
+        !window.confirm(
+          "You have made changes to a demo scenario. Do you want to save a copy?",
+        )
+      ) {
+        return;
+      }
+    }
+
     await scenario.value.io.saveToIndexedDb();
   }
 }
