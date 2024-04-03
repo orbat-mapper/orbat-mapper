1bc2167deeccc996a6a2f08bca0937535c63292b
Kjell Magne Fauske <kjellmf@gmail.com>
2024-04-03 06:39:09 +0200

    fix: hide waypoint timestamps by default

--- src/stores/geoStore.ts
+++ src/stores/geoStore.ts
@@ -100,7 +100,7 @@ export const useUnitSettingsStore = defineStore("unitSettings", {
       showHistory: true,
       editHistory: false,
       moveUnitEnabled: false,
-      showWaypointTimestamps: true,
+      showWaypointTimestamps: false,
     };
   },
 });
