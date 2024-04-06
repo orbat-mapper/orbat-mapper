4aab616f21f4fb94bade4e22b83bfd21f8c2760d
Kjell Magne Fauske <kjellmf@gmail.com>
2024-04-06 16:56:25 +0200

    style: make toggle smaller

--- src/components/ToggleField.vue
+++ src/components/ToggleField.vue
@@ -4,19 +4,19 @@
       v-model="enabled"
       :class="[
         enabled ? 'bg-indigo-600' : 'bg-gray-200',
-        'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
+        'flex w-8 cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
       ]"
     >
       <span
         aria-hidden="true"
         :class="[
-          enabled ? 'translate-x-5' : 'translate-x-0',
-          'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
+          enabled ? 'translate-x-3.5' : 'translate-x-0',
+          'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out',
         ]"
       />
     </Switch>
-    <SwitchLabel as="span" class="ml-3" v-if="$slots.default">
-      <span class="cursor-pointer text-sm font-medium text-gray-700"><slot></slot></span>
+    <SwitchLabel as="span" class="ml-3 text-sm" v-if="$slots.default">
+      <span class="cursor-pointer font-medium text-gray-700"><slot></slot></span>
     </SwitchLabel>
   </SwitchGroup>
 </template>
