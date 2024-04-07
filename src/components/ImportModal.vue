de7c0cc63476315d225f609e01697e5ede78364e
Kjell Magne Fauske <kjellmf@gmail.com>
2024-04-07 11:37:04 +0200

    refactor: make all import steps dynamic

--- src/components/ImportModal.vue
+++ src/components/ImportModal.vue
@@ -1,3 +1,74 @@
+<!--suppress JSDeprecatedSymbols -->
+<script setup lang="ts">
+import SimpleModal from "./SimpleModal.vue";
+import { useVModel } from "@vueuse/core";
+import ImportLoadStep from "@/components/ImportLoadStep.vue";
+import { defineAsyncComponent, ref, shallowRef } from "vue";
+import { OrbatGeneratorOrbat, SpatialIllusionsOrbat } from "@/types/externalModels";
+import type { FeatureCollection } from "geojson";
+import { MilxImportedLayer } from "@/composables/scenarioImport";
+import ImportImageStep from "@/components/ImportImageStep.vue";
+import { ImportedFileInfo } from "@/importexport/fileHandling";
+
+const ImportGeojsonStep = defineAsyncComponent(
+  () => import("@/components/ImportGeojsonStep.vue"),
+);
+
+const ImportSpatialIllusionsStep = defineAsyncComponent(
+  () => import("@/components/ImportSpatialIllusionsStep.vue"),
+);
+
+const ImportOrbatGeneratorStep = defineAsyncComponent(
+  () => import("@/components/ImportOrbatGeneratorStep.vue"),
+);
+
+const ImportMilxStep = defineAsyncComponent(
+  () => import("@/components/ImportMilxStep.vue"),
+);
+const ImportKMLStep = defineAsyncComponent(
+  () => import("@/components/ImportKMLStep.vue"),
+);
+const ImportSpreadsheetStep = defineAsyncComponent(
+  () => import("@/components/ImportSpreadsheetStep.vue"),
+);
+
+type ImportState =
+  | "select"
+  | "milx"
+  | "geojson"
+  | "unitgenerator"
+  | "orbatgenerator"
+  | "image"
+  | "kml"
+  | "xlsx";
+const importState = ref<ImportState>("select");
+const loadedData = shallowRef<any>([]);
+const fileInfo = shallowRef<ImportedFileInfo>();
+const props = withDefaults(defineProps<{ modelValue: boolean }>(), { modelValue: false });
+const emit = defineEmits(["update:modelValue", "cancel"]);
+
+const open = useVModel(props, "modelValue", emit);
+
+function onLoaded(nextState: ImportState, data: any, info: ImportedFileInfo) {
+  loadedData.value = data;
+  importState.value = nextState;
+  fileInfo.value = info;
+}
+
+function onImport() {
+  open.value = false;
+}
+
+function onCancel() {
+  open.value = false;
+  const objectUrlStates = ["image", "kml"];
+  if (objectUrlStates.includes(importState.value) && loadedData.value !== undefined) {
+    URL.revokeObjectURL(loadedData.value);
+  }
+  emit("cancel");
+}
+</script>
+
 <template>
   <SimpleModal v-model="open" dialog-title="Import data" @cancel="onCancel">
     <p class="text-sm leading-6 text-gray-500">Import data for use in your scenario</p>
@@ -52,66 +123,3 @@
     />
   </SimpleModal>
 </template>
-
-<script setup lang="ts">
-import SimpleModal from "./SimpleModal.vue";
-import { useNotifications } from "@/composables/notifications";
-import { useRouter } from "vue-router";
-import { useVModel } from "@vueuse/core";
-import ImportLoadStep from "@/components/ImportLoadStep.vue";
-import { defineAsyncComponent, ref, shallowRef } from "vue";
-import ImportGeojsonStep from "@/components/ImportGeojsonStep.vue";
-import ImportSpatialIllusionsStep from "@/components/ImportSpatialIllusionsStep.vue";
-import { OrbatGeneratorOrbat, SpatialIllusionsOrbat } from "@/types/externalModels";
-import type { FeatureCollection } from "geojson";
-import { MilxImportedLayer } from "@/composables/scenarioImport";
-import ImportOrbatGeneratorStep from "@/components/ImportOrbatGeneratorStep.vue";
-import ImportImageStep from "@/components/ImportImageStep.vue";
-import { ImportedFileInfo } from "@/importexport/fileHandling";
-
-const ImportMilxStep = defineAsyncComponent(
-  () => import("@/components/ImportMilxStep.vue"),
-);
-const ImportKMLStep = defineAsyncComponent(
-  () => import("@/components/ImportKMLStep.vue"),
-);
-const ImportSpreadsheetStep = defineAsyncComponent(
-  () => import("@/components/ImportSpreadsheetStep.vue"),
-);
-
-type ImportState =
-  | "select"
-  | "milx"
-  | "geojson"
-  | "unitgenerator"
-  | "orbatgenerator"
-  | "image"
-  | "kml"
-  | "xlsx";
-const importState = ref<ImportState>("select");
-const loadedData = shallowRef<any>([]);
-const fileInfo = shallowRef<ImportedFileInfo>();
-const props = withDefaults(defineProps<{ modelValue: boolean }>(), { modelValue: false });
-const emit = defineEmits(["update:modelValue", "cancel"]);
-
-const open = useVModel(props, "modelValue", emit);
-
-function onLoaded(nextState: ImportState, data: any, info: ImportedFileInfo) {
-  loadedData.value = data;
-  importState.value = nextState;
-  fileInfo.value = info;
-}
-
-function onImport() {
-  open.value = false;
-}
-
-function onCancel() {
-  open.value = false;
-  const objectUrlStates = ["image", "kml"];
-  if (objectUrlStates.includes(importState.value) && loadedData.value !== undefined) {
-    URL.revokeObjectURL(loadedData.value);
-  }
-  emit("cancel");
-}
-</script>
