4f0b33a6a2f737e416d281b24a41f52a758b18da
Kjell Magne Fauske <kjellmf@gmail.com>
2024-05-16 12:50:04 +0200

    refactor: create useBrowserScenarios composable

---
+++ src/composables/browserScenarios.ts
@@ -0,0 +1,125 @@
+import { computed, onMounted, ref } from "vue";
+import { ScenarioMetadata, useIndexedDb } from "@/scenariostore/localdb";
+import { MenuItemData } from "@/components/types";
+import { StoredScenarioAction } from "@/types/constants";
+import { MAP_EDIT_MODE_ROUTE } from "@/router/names";
+import { Scenario } from "@/types/scenarioModels";
+import { nanoid } from "@/utils";
+import { useRouter } from "vue-router";
+
+export const DEMO_SCENARIOS = [
+  {
+    name: "The Falklands War 1982",
+    id: "falkland82",
+    summary:
+      "The Falklands War was a military conflict that took place in 1982 between Argentina and the United Kingdom. Argentina invaded the Falkland Islands on April 2, 1982, and the UK responded by sending a task force to retake the islands.",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/8/8b/HMS_Broadsword_and_Hermes%2C_1982_%28IWM%29.jpg",
+  },
+  {
+    name: "Battles of Narvik 1940",
+    id: "narvik40",
+    summary:
+      "A series of naval and land engagements fought between German and Allied forces from April to June 1940. The battles marked the first Allied victory against Germany in the war.",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/5/5f/Norwegian_Army_Colt_heavy_machine_gun_at_the_Narvik_front.jpg",
+  },
+];
+
+export function useBrowserScenarios() {
+  const router = useRouter();
+  const storedScenarios = ref<ScenarioMetadata[]>([]);
+  const activeSort = ref("lastModified");
+
+  const sortOptions = computed<MenuItemData[]>(() => [
+    {
+      label: "Name",
+      action: () => {
+        storedScenarios.value.sort((a, b) => a.name.localeCompare(b.name));
+        activeSort.value = "name";
+      },
+      active: activeSort.value === "name",
+    },
+    {
+      label: "Last modified",
+      action: () => {
+        activeSort.value = "lastModified";
+        storedScenarios.value.sort((a, b) => +b.modified - +a.modified);
+      },
+      active: activeSort.value === "lastModified",
+    },
+    {
+      label: "Created",
+      action: () => {
+        activeSort.value = "created";
+        storedScenarios.value.sort((a, b) => +b.created - +a.created);
+      },
+      active: activeSort.value === "created",
+    },
+  ]);
+
+  async function onAction(action: StoredScenarioAction, scenario: ScenarioMetadata) {
+    const { deleteScenario, listScenarios, duplicateScenario, downloadAsJson } =
+      await useIndexedDb();
+    switch (action) {
+      case "open":
+        await router.push({
+          name: MAP_EDIT_MODE_ROUTE,
+          params: { scenarioId: scenario.id },
+        });
+        break;
+      case "delete":
+        if (
+          window.confirm(
+            `Are you sure you want to permanently delete the scenario "${scenario.name}"?`,
+          )
+        ) {
+          await deleteScenario(scenario.id);
+        }
+        break;
+      case "download":
+        await downloadAsJson(scenario.id);
+        break;
+      case "duplicate":
+        await duplicateScenario(scenario.id);
+        break;
+    }
+
+    await reloadScenarios();
+
+    async function reloadScenarios() {
+      storedScenarios.value = await listScenarios();
+      storedScenarios.value.reverse();
+    }
+  }
+
+  async function loadScenario(v: Scenario) {
+    const { addScenario, getScenarioInfo, putScenario } = await useIndexedDb();
+
+    const existingScenarioInfo = await getScenarioInfo(v.id ?? nanoid());
+    if (existingScenarioInfo) {
+      let scenarioId = v.id;
+      if (
+        window.confirm(
+          "A scenario with the same ID is stored in the browser. Do you want to replace it with this scenario?",
+        )
+      ) {
+        scenarioId = await putScenario(v);
+      } else {
+        scenarioId = await addScenario(v, nanoid());
+      }
+      await router.push({ name: MAP_EDIT_MODE_ROUTE, params: { scenarioId } });
+    } else {
+      const scenarioId = await addScenario(v);
+      await router.push({ name: MAP_EDIT_MODE_ROUTE, params: { scenarioId } });
+    }
+  }
+
+  onMounted(async () => {
+    const { listScenarios } = await useIndexedDb();
+    storedScenarios.value = await listScenarios();
+    storedScenarios.value.reverse();
+  });
+
+  return { storedScenarios, sortOptions, onAction, loadScenario };
+}
--- src/views/LandingPageScenarios.vue
+++ src/views/LandingPageScenarios.vue
@@ -40,7 +40,7 @@
     <section class="mx-auto max-w-7xl p-6">
       <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <li
-          v-for="scenario in scenarios"
+          v-for="scenario in DEMO_SCENARIOS"
           :key="scenario.name"
           class="col-span-1 flex flex-col divide-y divide-gray-200 overflow-hidden rounded-lg border bg-white text-center shadow focus-within:border-blue-800 hover:border-army"
         >
@@ -113,64 +113,13 @@ import WipBadge from "../components/WipBadge.vue";
 import { MAP_EDIT_MODE_ROUTE, NEW_SCENARIO_ROUTE } from "@/router/names";
 import LoadScenarioPanel from "@/modules/scenarioeditor/LoadScenarioPanel.vue";
 import { useScenario } from "@/scenariostore";
-import { Scenario } from "@/types/scenarioModels";
 import LoadScenarioFromUrlPanel from "@/modules/scenarioeditor/LoadScenarioFromUrlPanel.vue";
-import { ScenarioMetadata, useIndexedDb } from "@/scenariostore/localdb";
-import { computed, onMounted, ref } from "vue";
 import ScenarioLinkCard from "@/components/ScenarioLinkCard.vue";
-import { StoredScenarioAction } from "@/types/constants";
-import { nanoid } from "@/utils";
 import SortDropdown from "@/components/SortDropdown.vue";
-import { MenuItemData } from "@/components/types";
 import LoadScenarioFromLocalstoragePanel from "@/modules/scenarioeditor/LoadScenarioFromLocalstoragePanel.vue";
+import { DEMO_SCENARIOS, useBrowserScenarios } from "@/composables/browserScenarios";
 
-const storedScenarios = ref<ScenarioMetadata[]>([]);
-const activeSort = ref("lastModified");
-
-const sortOptions = computed<MenuItemData[]>(() => [
-  {
-    label: "Name",
-    action: () => {
-      storedScenarios.value.sort((a, b) => a.name.localeCompare(b.name));
-      activeSort.value = "name";
-    },
-    active: activeSort.value === "name",
-  },
-  {
-    label: "Last modified",
-    action: () => {
-      activeSort.value = "lastModified";
-      storedScenarios.value.sort((a, b) => +b.modified - +a.modified);
-    },
-    active: activeSort.value === "lastModified",
-  },
-  {
-    label: "Created",
-    action: () => {
-      activeSort.value = "created";
-      storedScenarios.value.sort((a, b) => +b.created - +a.created);
-    },
-    active: activeSort.value === "created",
-  },
-]);
-const scenarios = [
-  {
-    name: "The Falklands War 1982",
-    id: "falkland82",
-    summary:
-      "The Falklands War was a military conflict that took place in 1982 between Argentina and the United Kingdom. Argentina invaded the Falkland Islands on April 2, 1982, and the UK responded by sending a task force to retake the islands.",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/8/8b/HMS_Broadsword_and_Hermes%2C_1982_%28IWM%29.jpg",
-  },
-  {
-    name: "Battles of Narvik 1940",
-    id: "narvik40",
-    summary:
-      "A series of naval and land engagements fought between German and Allied forces from April to June 1940. The battles marked the first Allied victory against Germany in the war.",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/5/5f/Norwegian_Army_Colt_heavy_machine_gun_at_the_Narvik_front.jpg",
-  },
-];
+const { storedScenarios, sortOptions, onAction, loadScenario } = useBrowserScenarios();
 
 const router = useRouter();
 const getScenarioTo = (scenarioId: string) => {
@@ -186,71 +135,8 @@ const newScenario = () => {
 
 const { scenario } = useScenario();
 
-async function loadScenario(v: Scenario) {
-  const { addScenario, getScenarioInfo, putScenario } = await useIndexedDb();
-
-  const existingScenarioInfo = await getScenarioInfo(v.id ?? nanoid());
-  if (existingScenarioInfo) {
-    let scenarioId = v.id;
-    if (
-      window.confirm(
-        "A scenario with the same ID is stored in the browser. Do you want to replace it with this scenario?",
-      )
-    ) {
-      scenarioId = await putScenario(v);
-    } else {
-      scenarioId = await addScenario(v, nanoid());
-    }
-    await router.push({ name: MAP_EDIT_MODE_ROUTE, params: { scenarioId } });
-  } else {
-    const scenarioId = await addScenario(v);
-    await router.push({ name: MAP_EDIT_MODE_ROUTE, params: { scenarioId } });
-  }
-}
-
 async function loadFromLocalStorage() {
   scenario.value.io.loadFromLocalStorage();
   await loadScenario(scenario.value.io.serializeToObject());
-}
-
-onMounted(async () => {
-  const { listScenarios } = await useIndexedDb();
-  storedScenarios.value = await listScenarios();
-  storedScenarios.value.reverse();
-});
-
-async function onAction(action: StoredScenarioAction, scenario: ScenarioMetadata) {
-  const { deleteScenario, listScenarios, duplicateScenario, downloadAsJson } =
-    await useIndexedDb();
-  switch (action) {
-    case "open":
-      await router.push({
-        name: MAP_EDIT_MODE_ROUTE,
-        params: { scenarioId: scenario.id },
-      });
-      break;
-    case "delete":
-      if (
-        window.confirm(
-          `Are you sure you want to permanently delete the scenario "${scenario.name}"?`,
-        )
-      ) {
-        await deleteScenario(scenario.id);
-      }
-      break;
-    case "download":
-      await downloadAsJson(scenario.id);
-      break;
-    case "duplicate":
-      await duplicateScenario(scenario.id);
-      break;
-  }
-
-  await reloadScenarios();
-
-  async function reloadScenarios() {
-    storedScenarios.value = await listScenarios();
-    storedScenarios.value.reverse();
-  }
 }
 </script>
