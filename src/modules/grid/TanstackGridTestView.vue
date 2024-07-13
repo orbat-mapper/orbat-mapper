cd9c2b6930ace4c9e62c9c4d171ba75d0ffade7e
Kjell Magne Fauske <kjellmf@gmail.com>
2024-07-13 21:05:07 +0200

    refactor(wip): improve data grid demo

--- src/modules/grid/TanstackGridTestView.vue
+++ src/modules/grid/TanstackGridTestView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { NUnit } from "@/types/internalModels";
 import { computed, h, onMounted, ref } from "vue";
+import { NUnit } from "@/types/internalModels";
 import { useScenario } from "@/scenariostore";
 import { MenuItemData } from "@/components/types";
 import { SideAction, SideActions } from "@/types/constants";
@@ -8,32 +8,50 @@ import ToggleField from "@/components/ToggleField.vue";
 import BaseButton from "@/components/BaseButton.vue";
 import {
   ColumnDef,
-  getSortedRowModel,
-  RowSelectionState,
-  SortingState,
-} from "@tanstack/vue-table";
-import {
   createColumnHelper,
   FlexRender,
   getCoreRowModel,
+  getExpandedRowModel,
+  getFilteredRowModel,
+  getGroupedRowModel,
+  getSortedRowModel,
+  GroupingState,
+  RowSelectionState,
+  SortingState,
   useVueTable,
 } from "@tanstack/vue-table";
 
 import MilitarySymbol from "@/components/MilitarySymbol.vue";
 import DotsMenu from "@/components/DotsMenu.vue";
-import { ArrowSmallDownIcon, ArrowSmallUpIcon } from "@heroicons/vue/20/solid";
+import {
+  ArrowSmallDownIcon,
+  ArrowSmallUpIcon,
+  ChevronRightIcon,
+} from "@heroicons/vue/20/solid";
 import { useVirtualizer } from "@tanstack/vue-virtual";
+import { valueUpdater } from "@/modules/grid/helpers";
+import { useDebounce } from "@vueuse/core";
+import InputGroup from "@/components/InputGroup.vue";
 
 interface ExtendedUnit extends NUnit {
   sideName: string;
+  sideGroupName: string;
   sideId: string;
 }
 
-const rowSelection = ref<RowSelectionState>({});
+const { scenario } = useScenario();
 
+const doSelect = ref(true);
+const rowSelection = ref<RowSelectionState>({});
+const grouping = ref<GroupingState>([]);
+const query = ref("");
+const debouncedQuery = useDebounce(query, 200);
 const sorting = ref<SortingState>([]);
 const parentRef = ref<HTMLElement | null>(null);
 const columnHelper = createColumnHelper<ExtendedUnit>();
+const selected = ref<ExtendedUnit[]>([]);
+const data = ref<ExtendedUnit[]>([]);
+
 const columns = ref<ColumnDef<ExtendedUnit, any>[]>([
   columnHelper.display({
     id: "select",
@@ -65,15 +83,26 @@ const columns = ref<ColumnDef<ExtendedUnit, any>[]>([
     size: 80,
     header: "Icon",
     enableSorting: false,
-    cell: ({ row, getValue }) => {
-      return h(MilitarySymbol, { sidc: getValue(), size: 20 });
+    cell: ({ row, getValue, cell }) => {
+      return h(MilitarySymbol, {
+        sidc: getValue(),
+        size: 20,
+        modifiers: row.original.symbolOptions,
+      });
     },
   }),
-  columnHelper.accessor("name", { header: "Name" }),
-  columnHelper.accessor("shortName", { header: "Short name" }),
-  columnHelper.accessor("externalUrl", { header: "URL" }),
+  {
+    header: "Meta",
+    columns: [
+      columnHelper.accessor("name", { header: "Name" }),
+      columnHelper.accessor("shortName", { header: "Short name" }),
+      columnHelper.accessor("externalUrl", { header: "URL" }),
+    ],
+  },
+
   columnHelper.accessor("sideName", { header: "Side" }),
-  columnHelper.accessor("id", { header: "id" }),
+  columnHelper.accessor("sideGroupName", { header: "Side group" }),
+  columnHelper.accessor("id", { header: "id", enableGlobalFilter: false }),
   {
     header: "Position",
     enableSorting: false,
@@ -92,7 +121,6 @@ const columns = ref<ColumnDef<ExtendedUnit, any>[]>([
     },
   }),
 ]);
-const data = ref<ExtendedUnit[]>([]);
 
 const table = useVueTable({
   get data() {
@@ -102,19 +130,27 @@ const table = useVueTable({
     get rowSelection() {
       return rowSelection.value;
     },
+    get grouping() {
+      return grouping.value;
+    },
+    get globalFilter() {
+      return debouncedQuery.value;
+    },
   },
   enableRowSelection: true,
   columnResizeMode: "onChange",
   columns: columns.value,
   getCoreRowModel: getCoreRowModel(),
   getSortedRowModel: getSortedRowModel(),
+  getGroupedRowModel: getGroupedRowModel(),
+  getExpandedRowModel: getExpandedRowModel(),
+  getFilteredRowModel: getFilteredRowModel(),
+  autoResetAll: false,
+  autoResetExpanded: false,
   getRowId: (row) => row.id,
-  onRowSelectionChange: (updateOrValue) => {
-    rowSelection.value =
-      typeof updateOrValue === "function"
-        ? updateOrValue(rowSelection.value)
-        : updateOrValue;
-  },
+  onRowSelectionChange: (updateOrValue) => valueUpdater(updateOrValue, rowSelection),
+  onGroupingChange: (updateOrValue) => valueUpdater(updateOrValue, grouping),
+  onGlobalFilterChange: (updateOrValue) => valueUpdater(updateOrValue, query),
 });
 
 const rowVirtualizerOptions = computed(() => {
@@ -125,10 +161,10 @@ const rowVirtualizerOptions = computed(() => {
     overscan: 5,
   };
 });
-
 const rows = computed(() => {
   return table.getRowModel().rows;
 });
+
 const rowVirtualizer = useVirtualizer(rowVirtualizerOptions);
 
 const virtualRows = computed(() => rowVirtualizer.value.getVirtualItems());
@@ -144,20 +180,23 @@ const sideMenuItems: MenuItemData<SideAction>[] = [
   { label: "Move down", action: SideActions.MoveDown },
 ];
 
-const { scenario, isReady } = useScenario();
-
-const selected = ref<ExtendedUnit[]>([]);
-
 onMounted(async () => {
   await scenario.value.io.loadDemoScenario("falkland82");
 
   const { unitMap, sideGroupMap, sideMap } = scenario.value.store.state;
+  const { unitActions } = scenario.value;
   const unitData: ExtendedUnit[] = [];
   Object.keys(sideMap).forEach((sideId) =>
     scenario.value.unitActions.walkSide(
       sideId,
       (unit, level, parent, sideGroup, side) => {
-        unitData.push({ ...unit, sideId: side.id, sideName: side?.name });
+        unitData.push({
+          ...unit,
+          sideId: side.id,
+          sideName: side?.name,
+          sideGroupName: sideGroup.name,
+          symbolOptions: unitActions.getCombinedSymbolOptions(unit),
+        });
       },
     ),
   );
@@ -170,100 +209,135 @@ function onAction(action: SideAction, props: any) {
 }
 
 function mutateData() {
-  data.value.splice(10, 5);
-  data.value = [...data.value];
+  // data.value.splice(10, 5);
+  const copy = [...data.value];
+  copy[0].name = "Mutated";
+  data.value = copy;
   // data.value = data.value.slice().reverse();
 }
-
-const doSelect = ref(true);
 </script>
 <template>
-  <main class="h-full pt-10 sm:p-10">
-    <div class="fixed top-0 z-40 flex items-center gap-4 p-1">
-      <ToggleField v-model="doSelect">Select</ToggleField>
-      <span v-if="doSelect"
-        >Selected: {{ selected.length }} {{ Object.keys(rowSelection) }}</span
+  <main class="flex h-full flex-col">
+    <div class="flex shrink-0 items-center gap-4 p-4">
+      <span
+        >Selected: {{ selected.length }} {{ Object.keys(rowSelection) }}
+        {{ grouping }}</span
       >
       <BaseButton @click="mutateData()">Mutate</BaseButton>
+      <InputGroup v-model="query" placeholder="Search" />
     </div>
     <section
       class="relative h-full overflow-auto rounded-lg border shadow"
       ref="parentRef"
     >
-      <header class="sticky top-0 z-10">
-        <div
-          v-for="headerGroup in table.getHeaderGroups()"
-          :key="headerGroup.id"
-          class="flex divide-x divide-gray-200"
-        >
-          <div
-            v-for="header in headerGroup.headers"
-            :key="header.id"
-            :style="{
-              width: `${header.getSize()}px`,
-              minWidth: `${header.getSize()}px`,
-            }"
-            role="columnheader"
-            class="flex-0 relative flex w-full items-center justify-between overflow-hidden border-b bg-gray-100 px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
-            :class="{ 'cursor-pointer select-none': header.column.getCanSort() }"
-            @click="header.column.getToggleSortingHandler()?.($event)"
+      <table class="grid">
+        <thead class="sticky top-0 z-10">
+          <tr
+            v-for="headerGroup in table.getHeaderGroups()"
+            :key="headerGroup.id"
+            class="flex divide-x divide-gray-200"
           >
-            <span class="truncate"
-              ><FlexRender
-                v-if="!header.isPlaceholder"
-                :render="header.column.columnDef.header"
-                :props="header.getContext()"
-            /></span>
-            <span
-              v-if="header.column.getCanSort() && header.column.getIsSorted()"
-              class="flex-none rounded text-gray-700 group-hover:bg-gray-300"
+            <th
+              v-for="header in headerGroup.headers"
+              :key="header.id"
+              :style="{
+                width: `${header.getSize()}px`,
+              }"
+              role="columnheader"
+              class="flex-0 relative flex w-full select-none items-center justify-between overflow-hidden border-b bg-gray-100 px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
+              :class="{ 'cursor-pointer': header.column.getCanSort() }"
+              @click="header.column.getToggleSortingHandler()?.($event)"
             >
-              <ArrowSmallDownIcon
-                v-if="header.column.getIsSorted() === 'asc'"
-                class="h-5 w-5"
-                aria-hidden="true"
-              />
-              <ArrowSmallUpIcon
-                v-else-if="header.column.getIsSorted() === 'desc'"
-                class="h-5 w-5"
-                aria-hidden="true"
-              />
-            </span>
-            <div
-              v-if="header.column.getCanResize()"
-              @dblclick="header.column.resetSize()"
-              @mousedown="header.getResizeHandler()($event)"
-              @touchstart="header.getResizeHandler()($event)"
-              @click.stop
-              role="separator"
-              class="z-5 absolute right-0 top-0 h-full w-4 cursor-col-resize hover:bg-red-100 sm:w-2"
-            />
-          </div>
-          <div></div>
-        </div>
-      </header>
-      <div
-        class="tbody relative"
-        :style="{ height: `${rowVirtualizer.getTotalSize()}px` }"
-      >
-        <div
-          v-for="virtualRow in virtualRows"
-          :key="virtualRow.key"
-          :style="{ transform: `translateY(${virtualRow.start}px)` }"
-          class="group absolute flex h-10 w-full divide-x divide-gray-200 hover:bg-gray-50"
-        >
-          <div
-            v-for="cell in rows[virtualRow.index].getVisibleCells()"
-            :key="cell.id"
-            :style="{
-              width: `${cell.column.getSize()}px`,
-            }"
-            class="flex shrink-0 items-center overflow-hidden text-nowrap border-b p-4"
+              <template v-if="!header.isPlaceholder">
+                <span class="truncate"
+                  ><FlexRender
+                    :render="header.column.columnDef.header"
+                    :props="header.getContext()"
+                /></span>
+                <button
+                  v-if="header.column.getCanGroup()"
+                  @click.stop="header.column.getToggleGroupingHandler()()"
+                >
+                  GR
+                </button>
+                <span
+                  v-if="header.column.getCanSort() && header.column.getIsSorted()"
+                  class="flex-none rounded text-gray-700 group-hover:bg-gray-300"
+                >
+                  <ArrowSmallDownIcon
+                    v-if="header.column.getIsSorted() === 'asc'"
+                    class="h-5 w-5"
+                    aria-hidden="true"
+                  />
+                  <ArrowSmallUpIcon
+                    v-else-if="header.column.getIsSorted() === 'desc'"
+                    class="h-5 w-5"
+                    aria-hidden="true"
+                  />
+                </span>
+                <div
+                  v-if="header.column.getCanResize()"
+                  @dblclick="header.column.resetSize()"
+                  @mousedown="header.getResizeHandler()($event)"
+                  @touchstart="header.getResizeHandler()($event)"
+                  @click.stop
+                  role="separator"
+                  class="z-5 absolute right-0 top-0 h-full w-4 cursor-col-resize hover:bg-red-100 sm:w-2"
+                />
+              </template>
+            </th>
+          </tr>
+        </thead>
+        <tbody class="relative" :style="{ height: `${rowVirtualizer.getTotalSize()}px` }">
+          <tr
+            v-for="row in virtualRows"
+            :key="row.key"
+            :style="{ transform: `translateY(${row.start}px)` }"
+            class="group absolute flex h-10 w-full divide-x divide-gray-200 hover:bg-gray-50"
+            :data-index="row.index"
           >
-            <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
-          </div>
-        </div>
-      </div>
+            <td
+              v-for="(cell, idx) in rows[row.index].getVisibleCells()"
+              :key="cell.id"
+              :id="cell.id"
+              :style="{
+                width: `${cell.column.getSize()}px`,
+              }"
+              class="cell"
+              :data-index="idx"
+            >
+              <button
+                v-if="cell.getIsGrouped()"
+                @click="cell.row.toggleExpanded()"
+                class="flex items-center"
+              >
+                <ChevronRightIcon
+                  class="h-6 w-6 text-red-800 transition-transform group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-gray-100"
+                  :class="{
+                    'rotate-90': cell.row.getIsExpanded(),
+                  }"
+                />
+
+                <FlexRender
+                  :render="cell.column.columnDef.cell"
+                  :props="cell.getContext()"
+                />&nbsp; ({{ cell.row.subRows.length }})
+              </button>
+              <FlexRender
+                v-else-if="!cell.getIsPlaceholder()"
+                :render="cell.column.columnDef.cell"
+                :props="cell.getContext()"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   </main>
 </template>
+
+<style>
+.cell {
+  @apply flex shrink-0 items-center overflow-hidden text-nowrap border-b p-4;
+}
+</style>
