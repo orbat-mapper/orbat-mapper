9d42d2cdec49e49c31cb0cdc4b11b10a07561ed5
Kjell Magne Fauske <kjellmf@gmail.com>
2024-08-12 20:32:36 +0200

    fix: remove mobile-drag-drop polyfill
    The polyfill was causing some unwanted behaviour on mobile. This will unfortunately break drag and drop on Firefox for Android. (see https://github.com/atlassian/pragmatic-drag-and-drop/issues/14)

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,5 @@ import "inter-ui/inter-variable.css";
 import "./styles.css";
 import App from "./App.vue";
 import { router } from "./router";
-import { polyfill as dragDropPolyfill } from "mobile-drag-drop";
-
-// needed for some mobile devices (Firefox android)
-dragDropPolyfill();
 
 createApp(App).use(router).use(createPinia()).mount("#app");
