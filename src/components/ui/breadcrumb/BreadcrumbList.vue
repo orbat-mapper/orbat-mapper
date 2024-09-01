e985a6752aca751c24ed0cc1939047640cc31282
Kjell Magne Fauske <kjellmf@gmail.com>
2024-09-01 18:22:12 +0200

    chore: run prettier on shadcn-vue components

--- src/components/ui/breadcrumb/BreadcrumbList.vue
+++ src/components/ui/breadcrumb/BreadcrumbList.vue
@@ -1,15 +1,20 @@
 <script lang="ts" setup>
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@/lib/utils'
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/lib/utils";
 
 const props = defineProps<{
-  class?: HTMLAttributes['class']
-}>()
+  class?: HTMLAttributes["class"];
+}>();
 </script>
 
 <template>
   <ol
-    :class="cn('flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground sm:gap-2.5', props.class)"
+    :class="
+      cn(
+        'flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground sm:gap-2.5',
+        props.class,
+      )
+    "
   >
     <slot />
   </ol>
--- src/components/ui/breadcrumb/BreadcrumbSeparator.vue
+++ src/components/ui/breadcrumb/BreadcrumbSeparator.vue
@@ -1,19 +1,15 @@
 <script lang="ts" setup>
-import type { HTMLAttributes } from 'vue'
-import { ChevronRight } from 'lucide-vue-next'
-import { cn } from '@/lib/utils'
+import type { HTMLAttributes } from "vue";
+import { ChevronRight } from "lucide-vue-next";
+import { cn } from "@/lib/utils";
 
 const props = defineProps<{
-  class?: HTMLAttributes['class']
-}>()
+  class?: HTMLAttributes["class"];
+}>();
 </script>
 
 <template>
-  <li
-    role="presentation"
-    aria-hidden="true"
-    :class="cn('[&>svg]:size-3.5', props.class)"
-  >
+  <li role="presentation" aria-hidden="true" :class="cn('[&>svg]:size-3.5', props.class)">
     <slot>
       <ChevronRight />
     </slot>
