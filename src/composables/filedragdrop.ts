74a8c80a310fe0c3c5695c407e5284b7af830ead
Kjell Magne Fauske <kjellmf@gmail.com>
2024-09-24 20:06:23 +0200

    refactor: use pragmatic DnD for handling external files

--- src/composables/filedragdrop.ts
+++ src/composables/filedragdrop.ts
@@ -1,53 +1,36 @@
-/*
-Based on https://github.com/vueuse/vueuse/blob/main/packages/core/useDropZone/index.ts
-and https://github.com/vueuse/vueuse/issues/2057#issuecomment-1215027426
-
-
- */
-import { isClient, MaybeRefOrGetter, useEventListener } from "@vueuse/core";
-import { ref, type Ref } from "vue";
+import { onMounted, onUnmounted, ref, type Ref } from "vue";
+import { dropTargetForExternal } from "@atlaskit/pragmatic-drag-and-drop/external/adapter";
+import { containsFiles, getFiles } from "@atlaskit/pragmatic-drag-and-drop/external/file";
 
 export interface UseFileDropZoneReturn {
   isOverDropZone: Ref<boolean>;
 }
 
-function hasFiles(dataTransfer: DataTransfer): boolean {
-  return (
-    dataTransfer.items &&
-    Array.from(dataTransfer.items).some((item) => item.kind === "file")
-  );
-}
-
 export function useFileDropZone(
-  target: MaybeRefOrGetter<HTMLElement | null | undefined>,
-  onDrop?: (files: File[] | null) => void,
+  target: Ref<HTMLElement | null | undefined>,
+  onDropHandler?: (files: File[] | null) => void,
 ): UseFileDropZoneReturn {
   const isOverDropZone = ref(false);
-  let counter = 0;
 
-  if (isClient) {
-    useEventListener<DragEvent>(target, "dragenter", (event) => {
-      event.preventDefault();
-      counter += 1;
-      isOverDropZone.value = event.dataTransfer ? hasFiles(event.dataTransfer) : false;
+  let cleanup: () => void;
+  onMounted(() => {
+    if (!target.value) return;
+    cleanup = dropTargetForExternal({
+      element: target.value,
+      canDrop: containsFiles,
+      onDragEnter: () => (isOverDropZone.value = true),
+      onDragLeave: () => (isOverDropZone.value = false),
+      onDrop({ source }) {
+        const files: File[] = getFiles({ source });
+        isOverDropZone.value = false;
+        onDropHandler?.(files.length === 0 ? null : files);
+      },
     });
-    useEventListener<DragEvent>(target, "dragover", (event) => {
-      event.preventDefault();
-    });
-    useEventListener<DragEvent>(target, "dragleave", (event) => {
-      event.preventDefault();
-      counter -= 1;
-      if (counter === 0) isOverDropZone.value = false;
-    });
-    useEventListener<DragEvent>(target, "drop", (event) => {
-      event.preventDefault();
-      if (isOverDropZone.value === false) return;
-      counter = 0;
-      isOverDropZone.value = false;
-      const files = Array.from(event.dataTransfer?.files ?? []);
-      onDrop?.(files.length === 0 ? null : files);
-    });
-  }
+  });
+
+  onUnmounted(() => {
+    cleanup();
+  });
 
   return {
     isOverDropZone,
