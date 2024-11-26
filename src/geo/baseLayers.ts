4b1a9052ffdf5f463b79b063f8255aa8833b2893
Kjell Magne Fauske <kjellmf@gmail.com>
2024-11-26 18:59:55 +0100

    refactor: make base layers easier to configure

--- src/geo/baseLayers.ts
+++ src/geo/baseLayers.ts
@@ -1,123 +1,138 @@
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
+import type { Options as TileLayerOptions } from "ol/layer/BaseTile";
 import type { Options as OSMOptions } from "ol/source/OSM";
 import OSM from "ol/source/OSM";
 import type { Options as XYZOptions } from "ol/source/XYZ";
 import XYZ from "ol/source/XYZ";
 import { transformExtent } from "ol/proj";
+import { klona } from "klona";
 
-const osmOptions: OSMOptions = {
-  crossOrigin: "anonymous",
-};
+interface BaseLayerConfig {
+  title: string;
+  name: string;
+  layerSourceType?: "osm" | "xyz";
+  layerType: "baselayer";
+  tileLayerConfig?: Exclude<TileLayerOptions<any>, "source" | "map" | "properties">;
+}
 
-const osmDEOptions: OSMOptions = {
-  url: "https://tile.openstreetmap.de/{z}/{x}/{y}.png",
-  crossOrigin: "anonymous",
-};
+interface XYZLayerConfig extends BaseLayerConfig {
+  layerSourceType: "xyz";
+  sourceOptions: XYZOptions;
+}
 
-const grayBasemapOptions: XYZOptions = {
-  attributions:
-    'Tiles © <a href="https://services.arcgisonline.com/ArcGIS/' +
-    'rest/services/World_Light_Gray_Base/MapServer">ArcGIS</a>',
-  url:
-    "https://server.arcgisonline.com/ArcGIS/rest/services/" +
-    "Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
-  crossOrigin: "anonymous",
-};
+interface OSMLayerConfig extends BaseLayerConfig {
+  layerSourceType: "osm";
+  sourceOptions: OSMOptions;
+}
 
-const openTopoMapOptions: XYZOptions = {
-  url: "https://a.tile.opentopomap.org/{z}/{x}/{y}.png",
-  maxZoom: 14,
-  crossOrigin: "anonymous",
-  attributions:
-    'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-};
+type LayerConfig = XYZLayerConfig | OSMLayerConfig;
 
-const esriWorldImageryOptions: XYZOptions = {
-  crossOrigin: "anonymous",
-  transition: 0, // should be set to 0 when opacity is < 1
-  attributions:
-    "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
-  url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-};
-
-const kartverketTopo4Options: XYZOptions = {
-  crossOrigin: "anonymous",
-  url: "https://cache.kartverket.no/v1/wmts/1.0.0/topo/default/webmercator/{z}/{y}/{x}.png",
-  attributions: '<a href="http://www.kartverket.no/">Kartverket</a>',
-};
+const layers: LayerConfig[] = [
+  {
+    title: "OSM",
+    name: "osm",
+    layerType: "baselayer",
+    layerSourceType: "osm",
+    sourceOptions: {
+      crossOrigin: "anonymous",
+    },
+  },
+  {
+    title: "OSM (DE)",
+    name: "osm-de",
+    layerType: "baselayer",
+    layerSourceType: "osm",
+    sourceOptions: {
+      url: "https://tile.openstreetmap.de/{z}/{x}/{y}.png",
+      crossOrigin: "anonymous",
+    },
+  },
+  {
+    title: "Gray Basemap",
+    name: "grayBasemap",
+    layerType: "baselayer",
+    layerSourceType: "xyz",
+    sourceOptions: {
+      attributions:
+        'Tiles © <a href="https://services.arcgisonline.com/ArcGIS/' +
+        'rest/services/World_Light_Gray_Base/MapServer">ArcGIS</a>',
+      url:
+        "https://server.arcgisonline.com/ArcGIS/rest/services/" +
+        "Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
+      crossOrigin: "anonymous",
+    },
+  },
+  {
+    title: "Open topo map",
+    name: "openTopoMap",
+    layerType: "baselayer",
+    layerSourceType: "xyz",
+    sourceOptions: {
+      url: "https://a.tile.opentopomap.org/{z}/{x}/{y}.png",
+      maxZoom: 14,
+      crossOrigin: "anonymous",
+      attributions:
+        'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    },
+  },
+  {
+    title: "ESRI World imagery",
+    name: "esriWorldImagery",
+    layerType: "baselayer",
+    layerSourceType: "xyz",
+    sourceOptions: {
+      crossOrigin: "anonymous",
+      transition: 0, // should be set to 0 when opacity is < 1
+      attributions:
+        "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+      url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    },
+  },
+  {
+    title: "Topographic Map Norway",
+    name: "kartverketTopo4",
+    layerType: "baselayer",
+    layerSourceType: "xyz",
+    sourceOptions: {
+      crossOrigin: "anonymous",
+      url: "https://cache.kartverket.no/v1/wmts/1.0.0/topo/default/webmercator/{z}/{y}/{x}.png",
+      attributions: '<a href="http://www.kartverket.no/">Kartverket</a>',
+    },
+    tileLayerConfig: {
+      extent: [2, 57, 33, 72],
+    },
+  },
+];
 
 export async function createBaseLayers(view: View, currentBaseLayerName = "osm") {
-  const openStreetMapLayer = new TileLayer({
-    source: new OSM(osmOptions),
-    visible: currentBaseLayerName === "osm",
-    preload: Infinity,
-    properties: {
-      title: "OSM",
-      name: "osm",
-      layerType: "baselayer",
-    },
-  });
+  return layers.map((layerConfig) => {
+    const { layerSourceType, layerType, title, name, tileLayerConfig } =
+      klona(layerConfig);
 
-  const openStreetMapLayerDE = new TileLayer({
-    source: new OSM(osmDEOptions),
-    visible: currentBaseLayerName === "osm-de",
-    preload: Infinity,
-    properties: {
-      title: "OSM (DE)",
-      name: "osm-de",
-      layerType: "baselayer",
-    },
-  });
-  const lightGrayLayer = new TileLayer({
-    preload: Infinity,
-    visible: currentBaseLayerName === "grayBasemap",
-    source: new XYZ(grayBasemapOptions),
-    properties: {
-      title: "Gray Basemap",
-      name: "grayBasemap",
-      layerType: "baselayer",
-    },
-  });
+    if (tileLayerConfig?.extent) {
+      tileLayerConfig.extent = transformExtent(
+        tileLayerConfig.extent,
+        "EPSG:4326",
+        view.getProjection(),
+      );
+    }
 
-  const openTopoMap = new TileLayer({
-    source: new XYZ(openTopoMapOptions),
-    visible: currentBaseLayerName === "openTopoMap",
-    properties: {
-      title: "Open topo map",
-      name: "openTopoMap",
-      layerType: "baselayer",
-    },
-  });
+    const properties = { title, name, layerType };
 
-  const esriWorldImagery = new TileLayer({
-    preload: Infinity,
-    visible: currentBaseLayerName === "esriWorldImagery",
-    source: new XYZ(esriWorldImageryOptions),
-    properties: {
-      name: "esriWorldImagery",
-      title: "ESRI World imagery",
-      layerType: "baselayer",
-    },
-  });
+    let source;
+    if (layerSourceType === "osm") {
+      source = new OSM(layerConfig.sourceOptions);
+    } else if (layerSourceType === "xyz") {
+      source = new XYZ(layerConfig.sourceOptions);
+    }
 
-  const kartverketTopo4 = new TileLayer({
-    visible: currentBaseLayerName === "kartverketTopo4",
-    source: new XYZ(kartverketTopo4Options),
-    properties: {
-      title: "Topographic Map Norway",
-      name: "kartverketTopo4",
-      layerType: "baselayer",
-    },
-    extent: transformExtent([2, 57, 33, 72], "EPSG:4326", view.getProjection()),
+    return new TileLayer({
+      source,
+      properties,
+      visible: currentBaseLayerName === layerConfig.name,
+      preload: Infinity,
+      ...tileLayerConfig,
+    });
   });
-
-  return [
-    openStreetMapLayer,
-    openStreetMapLayerDE,
-    lightGrayLayer,
-    openTopoMap,
-    esriWorldImagery,
-    kartverketTopo4,
-  ];
 }
