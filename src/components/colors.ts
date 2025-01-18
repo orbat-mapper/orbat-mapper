d1c2a53f8cbaffdfc9fbbd2eafe41e20ce265254
Kjell Magne Fauske <kjellmf@gmail.com>
2025-01-18 17:44:47 +0100

    refactor: put default colors into a separate file

---
+++ src/components/colors.ts
@@ -0,0 +1,50 @@
+export const defaultColors = [
+  {
+    name: "Black",
+    value: "Black",
+    bgColor: "bg-black",
+    selectedColor: "ring-black",
+  },
+  {
+    name: "Slate",
+    value: "#64748b",
+    bgColor: "bg-slate-500",
+    selectedColor: "ring-slate-500",
+  },
+  {
+    name: "Purple",
+    value: "#a855f7",
+    bgColor: "bg-purple-500",
+    selectedColor: "ring-purple-500",
+  },
+  {
+    name: "Blue",
+    value: "#3b82f6",
+    bgColor: "bg-blue-500",
+    selectedColor: "ring-blue-500",
+  },
+  {
+    name: "Green",
+    value: "#22c55e",
+    bgColor: "bg-green-500",
+    selectedColor: "ring-green-500",
+  },
+  {
+    name: "Yellow",
+    value: "#eab308",
+    bgColor: "bg-yellow-500",
+    selectedColor: "ring-yellow-500",
+  },
+  {
+    name: "Pink",
+    value: "#ec4899",
+    bgColor: "bg-pink-500",
+    selectedColor: "ring-pink-500",
+  },
+  {
+    name: "Rose",
+    value: "#f43f5e",
+    bgColor: "bg-rose-500",
+    selectedColor: "ring-rose-500",
+  },
+];
