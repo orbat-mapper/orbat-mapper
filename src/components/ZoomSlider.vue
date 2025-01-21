63fc9290d66de1ffeccf8a63c100189f74ef953f
KM <kjellmf@gmail.com>
2025-01-21 06:24:21 +0100

    style: prevent Safari from showing select cursor on slider thumb

--- src/components/ZoomSlider.vue
+++ src/components/ZoomSlider.vue
@@ -42,7 +42,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
     <SliderThumb
       v-for="(_, key) in modelValue"
       :key="key"
-      class="flex h-6 w-6 items-center justify-center rounded-full border-2 border-primary bg-background text-center text-xs ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+      class="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full border-2 border-primary bg-background text-center text-xs ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
       >{{ _ }}</SliderThumb
     >
   </SliderRoot>
