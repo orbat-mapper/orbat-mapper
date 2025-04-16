a459d96c6c7f50667e3667df365c05ef99c929af
Kjell Magne Fauske <kjellmf@gmail.com>
2025-04-16 06:41:03 +0200

    refactor: add select component from shadcn

---
+++ src/components/ui/select/SelectItemText.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { SelectItemText, type SelectItemTextProps } from "reka-ui";
+
+const props = defineProps<SelectItemTextProps>();
+</script>
+
+<template>
+  <SelectItemText data-slot="select-item-text" v-bind="props">
+    <slot />
+  </SelectItemText>
+</template>
