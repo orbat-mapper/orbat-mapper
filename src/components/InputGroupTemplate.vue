0cfca3f784428ddd12ee8366ecb9803fab18d50d
Kjell Magne Fauske <kjellmf@gmail.com>
2025-04-18 08:08:11 +0200

    refactor: convert InputGroupTemplate component

--- src/components/InputGroupTemplate.vue
+++ src/components/InputGroupTemplate.vue
@@ -1,15 +1,20 @@
+<script setup lang="ts">
+import { useId } from "vue";
+import { Label } from "@/components/ui/label";
+
+const props = defineProps<{ label?: string; description?: string; hint?: string }>();
+
+const id = useId();
+</script>
 <template>
   <div>
     <div class="flex justify-between">
-      <label
-        :for="id"
-        class="block text-sm font-medium text-gray-800 dark:text-slate-200"
-      >
+      <Label :for="id">
         <slot name="label">{{ label }}</slot>
-      </label>
+      </Label>
       <div v-if="hint || $slots.hint">
         <slot name="hint">
-          <span class="text-sm leading-6 text-gray-500">
+          <span class="text-muted-foreground text-sm leading-6">
             {{ hint }}
           </span>
         </slot>
@@ -18,16 +23,11 @@
     <div class="mt-1">
       <slot :id="id"></slot>
     </div>
-    <p v-if="description || $slots.description" class="mt-2 text-sm text-gray-500">
+    <p
+      v-if="description || $slots.description"
+      class="text-muted-foreground mt-2 text-sm"
+    >
       <slot name="description">{{ description }}</slot>
     </p>
   </div>
 </template>
-
-<script setup lang="ts">
-import { nanoid } from "nanoid";
-
-const props = defineProps<{ label?: string; description?: string; hint?: string }>();
-
-const id = nanoid();
-</script>
