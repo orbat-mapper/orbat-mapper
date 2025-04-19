1cb745c128b01ff33dc9d6d85ed0875cfdd67b8e
Kjell Magne Fauske <kjellmf@gmail.com>
2025-04-19 08:03:55 +0200

    Revert "refactor: replace native checkbox with custom Checkbox component"
    This reverts commit b4fb24d8317ee66183a20daa40c22c1d762e5b11.

--- src/components/InputCheckbox.vue
+++ src/components/InputCheckbox.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="relative flex items-start">
     <div class="flex h-5 items-center">
-      <Checkbox v-model="localValue" :id="_id" v-bind="$attrs" />
+      <input
+        type="checkbox"
+        v-model="localValue"
+        :id="_id"
+        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 disabled:opacity-50"
+        v-bind="$attrs"
+      />
     </div>
     <div class="ml-3 text-sm">
-      <label :for="_id" class="font-medium">
+      <label :for="_id" class="font-medium text-slate-700 dark:text-slate-300">
         <slot name="label">{{ label }}</slot>
       </label>
-      <p v-if="description || $slots.description" class="text-muted-foreground">
+      <p v-if="description || $slots.description" class="text-gray-500">
         <slot name="description">{{ description }}</slot>
       </p>
     </div>
@@ -15,20 +21,24 @@
 </template>
 
 <script setup lang="ts">
-import { useId } from "vue";
-import { Checkbox } from "@/components/ui/checkbox";
+import { computed } from "vue";
+import { nanoid } from "nanoid";
 
 interface Props {
   id?: string;
   label?: string;
   description?: string;
-  modelValue?: boolean;
+  modelValue?: boolean | any[];
 }
 const props = withDefaults(defineProps<Props>(), {});
-const localValue = defineModel<boolean>();
 const emit = defineEmits(["update:modelValue"]);
 
-const _id = props.id ?? useId();
+const _id = props.id || nanoid(5);
+
+const localValue = computed({
+  get: () => props.modelValue,
+  set: (value) => emit("update:modelValue", value),
+});
 </script>
 
 <script lang="ts">
