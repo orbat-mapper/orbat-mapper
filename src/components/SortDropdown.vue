2f8180c5910ecda91671cac0e14661f5dac1b068
Kjell Magne Fauske <kjellmf@gmail.com>
2025-05-01 15:03:25 +0200

    refactor: update SortDropdown component to use shadcn

--- src/components/SortDropdown.vue
+++ src/components/SortDropdown.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
-import { ChevronDownIcon } from "@heroicons/vue/20/solid";
-import { Float } from "@headlessui-float/vue";
 import { type MenuItemData } from "@/components/types";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-vue-next";
+import { Button } from "@/components/ui/button";
 
 const props = defineProps<{ options: MenuItemData[] }>();
 const emit = defineEmits(["action"]);
@@ -14,54 +19,21 @@ const onItemClick = (item: MenuItemData<string | Function>) => {
 </script>
 
 <template>
-  <Menu as="div" class="relative inline-block text-left">
-    <Float
-      placement="bottom-end"
-      :offset="8"
-      shift
-      enter="transition duration-200 ease-out"
-      enter-from="scale-95 opacity-0"
-      enter-to="scale-100 opacity-100"
-      leave="transition duration-150 ease-in"
-      leave-from="scale-100 opacity-100"
-      leave-to="scale-95 opacity-0"
-      tailwindcss-origin-class
-    >
-      <div>
-        <MenuButton
-          class="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900"
-        >
-          Sort
-          <ChevronDownIcon
-            class="-mr-1 ml-1 h-5 w-5 shrink-0 text-gray-400 group-hover:text-gray-500"
-            aria-hidden="true"
-          />
-        </MenuButton>
-      </div>
-
-      <MenuItems
-        class="ring-opacity-5 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black focus:outline-hidden"
+  <DropdownMenu>
+    <DropdownMenuTrigger as="child" class="" @click.stop>
+      <Button variant="ghost" class="">
+        Sort
+        <ChevronDown />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem
+        v-for="item in options"
+        @select="onItemClick(item)"
+        :disabled="item.disabled"
       >
-        <div class="py-1">
-          <MenuItem
-            v-for="option in options"
-            :key="option.label"
-            v-slot="{ active }"
-            @click="onItemClick(option)"
-          >
-            <button
-              type="button"
-              :class="[
-                option.active ? 'font-medium text-gray-900' : 'text-gray-500',
-                active ? 'bg-gray-100' : '',
-                'inline-flex w-full px-4 py-2 text-sm',
-              ]"
-            >
-              {{ option.label }}
-            </button>
-          </MenuItem>
-        </div>
-      </MenuItems>
-    </Float></Menu
-  >
+        <span>{{ item.label }}</span>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
 </template>
