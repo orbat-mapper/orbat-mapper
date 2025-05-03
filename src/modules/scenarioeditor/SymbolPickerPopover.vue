5a20bd818f710e3935a4bb49d6c628da51ac44b6
Kjell Magne Fauske <kjellmf@gmail.com>
2025-05-03 08:39:05 +0200

    refactor: remove @headlessui-float/vue dependency

--- src/modules/scenarioeditor/SymbolPickerPopover.vue
+++ src/modules/scenarioeditor/SymbolPickerPopover.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
 import { IconChevronUp, IconPlus as AddSymbolIcon } from "@iconify-prerendered/vue-mdi";
 import PanelSymbolButton from "@/components/PanelSymbolButton.vue";
-import { Float } from "@headlessui-float/vue";
-import {
-  Popover,
-  PopoverButton,
-  PopoverPanel,
-  RadioGroup,
-  RadioGroupOption,
-} from "@headlessui/vue";
-import FloatingPanel from "@/components/FloatingPanel.vue";
-import { type Ref, ref } from "vue";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+
+import { RadioGroup, RadioGroupOption } from "@headlessui/vue";
+import { ref } from "vue";
 import { useToolbarUnitSymbolData } from "@/composables/mainToolbarData";
 import { useMainToolbarStore } from "@/stores/mainToolbarStore";
 import { type UnitSymbolOptions } from "@/types/scenarioModels";
@@ -20,10 +14,11 @@ import MilitarySymbol from "@/components/MilitarySymbol.vue";
 import PanelButton from "@/components/PanelButton.vue";
 import DotsMenu from "@/components/DotsMenu.vue";
 import type { MenuItemData } from "@/components/types.ts";
+import { Button } from "@/components/ui/button";
 
 interface Props {
   symbolOptions: UnitSymbolOptions;
-  addUnit: (sidc: string, closePopover?: (ref?: Ref | HTMLElement) => void) => void;
+  addUnit: (sidc: string) => void;
 }
 
 const props = defineProps<Props>();
@@ -33,6 +28,7 @@ const { getModalSidc } = injectStrict(sidcModalKey);
 const { iconItems, customIcon, customSidc, symbolPage } = useToolbarUnitSymbolData();
 const store = useMainToolbarStore();
 
+const isOpen = ref(false);
 const symbolTabs = ref([
   { title: "Land", sidc: "30031000001211000000", id: "land" },
   { title: "Sea", sidc: "10033000001201000000", id: "sea" },
@@ -43,7 +39,7 @@ const panelItems: MenuItemData[] = [
   { label: "Add symbol to panel", action: () => handleChangeSymbol() },
 ];
 
-async function handleChangeSymbol(close?: (ref?: Ref | HTMLElement) => void) {
+async function handleChangeSymbol() {
   const newSidcValue = await getModalSidc(customSidc.value, {
     title: "Select symbol",
     hideModifiers: true,
@@ -52,85 +48,90 @@ async function handleChangeSymbol(close?: (ref?: Ref | HTMLElement) => void) {
   });
   if (newSidcValue !== undefined) {
     customIcon.value.code = newSidcValue.sidc;
-    props.addUnit(customSidc.value, close);
+    props.addUnit(customSidc.value);
+    isOpen.value = false;
   }
+}
+
+function onAddUnit(sidc: string) {
+  props.addUnit(sidc);
+  isOpen.value = false;
 }
 </script>
 
 <template>
-  <Popover as="template" v-slot="{ open, close }">
-    <Float placement="top" :offset="12" flip shift strategy="fixed">
-      <PopoverButton
-        title="Select icons"
-        class="ml-1 rounded p-2 text-sm text-gray-700 hover:bg-gray-200 disabled:opacity-50"
-        @click="store.clearToolbar()"
-      >
-        <IconChevronUp class="h-6 w-6" :class="{ 'scale-150 text-red-800': open }" />
-      </PopoverButton>
-      <PopoverPanel focus v-slot="{ close }">
-        <FloatingPanel class="overflow-hidden p-2">
-          <header class="-mx-2 -mt-2 flex items-center justify-between border border-b">
-            <RadioGroup
-              v-model="symbolPage"
-              class="isolate flex divide-x divide-gray-200 shadow-sm focus:outline-hidden"
+  <Popover v-model:open="isOpen">
+    <PopoverTrigger title="Select icons" @click="store.clearToolbar()" as-child>
+      <Button variant="ghost" size="icon">
+        <IconChevronUp class="size-6" :class="{ 'scale-150 text-red-800': isOpen }" />
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent
+      class="overflow-hidden p-2"
+      align="center"
+      side="top"
+      :sideOffset="10"
+    >
+      <header class="-mx-2 -mt-2 flex items-center justify-between border border-b">
+        <RadioGroup
+          v-model="symbolPage"
+          class="isolate flex divide-x divide-gray-200 shadow-sm focus:outline-hidden"
+        >
+          <RadioGroupOption
+            as="template"
+            v-for="{ id, sidc, title } in symbolTabs"
+            :key="id"
+            v-slot="{ checked, active }"
+            :value="id"
+          >
+            <div
+              :class="[
+                checked ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
+                'group relative min-w-0 justify-self-center overflow-hidden bg-white px-4 py-2 hover:bg-gray-50 focus:z-10 focus:outline-hidden',
+              ]"
             >
-              <RadioGroupOption
-                as="template"
-                v-for="{ id, sidc, title } in symbolTabs"
-                :key="id"
-                v-slot="{ checked, active }"
-                :value="id"
-              >
-                <div
-                  :class="[
-                    checked ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
-                    'group relative min-w-0 justify-self-center overflow-hidden bg-white px-4 py-2 hover:bg-gray-50 focus:z-10 focus:outline-hidden',
-                  ]"
-                >
-                  <MilitarySymbol
-                    :sidc="sidc"
-                    :title="title"
-                    :size="15"
-                    :options="{
-                      monoColor: checked ? 'black' : 'black',
-                      strokeWidth: 8,
-                    }"
-                  />
-                  <span
-                    aria-hidden="true"
-                    :class="[
-                      checked ? 'bg-army' : 'bg-transparent',
-                      'absolute inset-x-0 bottom-0 h-0.5',
-                    ]"
-                  />
-                </div>
-              </RadioGroupOption>
-            </RadioGroup>
-            <DotsMenu :items="panelItems" class="" />
-          </header>
-          <div class="mt-3 grid h-20 grid-cols-5 place-items-center items-center gap-2">
-            <PanelSymbolButton
-              class=""
-              v-for="{ sidc, text } in iconItems"
-              :key="sidc"
-              :sidc="sidc"
-              :title="text"
-              :symbol-options="symbolOptions"
-              @click="addUnit(sidc, close)"
-            />
-            <PanelSymbolButton
-              class=""
-              :sidc="customSidc"
-              :title="customIcon.text"
-              :symbol-options="symbolOptions"
-              @click="addUnit(customSidc, close)"
-            />
-            <PanelButton @click="handleChangeSymbol(close)" title="Add symbol">
-              <AddSymbolIcon class="h-5 w-5" />
-            </PanelButton>
-          </div>
-        </FloatingPanel>
-      </PopoverPanel>
-    </Float>
+              <MilitarySymbol
+                :sidc="sidc"
+                :title="title"
+                :size="15"
+                :options="{
+                  monoColor: checked ? 'black' : 'black',
+                  strokeWidth: 8,
+                }"
+              />
+              <span
+                aria-hidden="true"
+                :class="[
+                  checked ? 'bg-army' : 'bg-transparent',
+                  'absolute inset-x-0 bottom-0 h-0.5',
+                ]"
+              />
+            </div>
+          </RadioGroupOption>
+        </RadioGroup>
+        <DotsMenu :items="panelItems" class="" />
+      </header>
+      <div class="mt-3 grid h-20 grid-cols-5 place-items-center items-center gap-2">
+        <PanelSymbolButton
+          class=""
+          v-for="{ sidc, text } in iconItems"
+          :key="sidc"
+          :sidc="sidc"
+          :title="text"
+          :symbol-options="symbolOptions"
+          @click="onAddUnit(sidc)"
+        />
+        <PanelSymbolButton
+          class=""
+          :sidc="customSidc"
+          :title="customIcon.text"
+          :symbol-options="symbolOptions"
+          @click="onAddUnit(customSidc)"
+        />
+        <PanelButton @click="handleChangeSymbol()" title="Add symbol">
+          <AddSymbolIcon class="h-5 w-5" />
+        </PanelButton>
+      </div>
+    </PopoverContent>
   </Popover>
 </template>
