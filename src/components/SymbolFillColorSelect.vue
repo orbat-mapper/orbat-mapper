a3ba404f825b489e2972db6a62922b40c0131794
Kjell Magne Fauske <kjellmf@gmail.com>
2025-10-17 19:46:39 +0200

    Refactor SymbolFillColorSelect and PopoverColorPicker components

--- src/components/SymbolFillColorSelect.vue
+++ src/components/SymbolFillColorSelect.vue
@@ -1,41 +1,21 @@
-<template>
-  <SymbolCodeSelect label="Fill color" :items="colorIconItems" v-model="colorValue" />
-</template>
-
 <script setup lang="ts">
 import { type NullableSymbolItem } from "@/types/constants";
 import { computed } from "vue";
 import SymbolCodeSelect from "@/components/SymbolCodeSelect.vue";
+import { SYMBOL_FILL_COLORS } from "@/config/colors.ts";
 
 interface Props {
-  modelValue?: string | null;
   sid?: string;
   defaultFillColor?: string;
 }
 
-const props = withDefaults(defineProps<Props>(), { sid: "3", modelValue: "" });
+const props = withDefaults(defineProps<Props>(), { sid: "3" });
 const emit = defineEmits(["update:modelValue"]);
 
 const colorValue = defineModel<string | null>({ default: null });
 
-const colors: Omit<NullableSymbolItem, "sidc">[] = [
-  { code: null, text: "Default" },
-  { code: "#80e0ff", text: "Blue (standard)" },
-  { code: "#ff8080", text: "Red (standard)" },
-  { code: "#aaffaa", text: "Green (standard)" },
-  { code: "#ffff80", text: "Yellow (standard)" },
-  { code: "#ffa1ff", text: "Pink (civilian)" },
-  { code: "#aab074", text: "Olive" },
-  { code: "#5baa5b", text: "Infantry (Battle Order)" },
-  { code: "#ffd00b", text: "Armor (Battle Order)" },
-  { code: "#ff3333", text: "Artillery (Battle Order)" },
-  { code: "#f7f7f7", text: "Combat Support (Battle Order)" },
-  { code: "#d87600", text: "Service Support (Battle Order)" },
-  { code: "#a2e3e8", text: "Aviation (Battle Order)" },
-];
-
 const colorIconItems = computed((): NullableSymbolItem[] =>
-  colors.map((item) => ({
+  [{ code: null, text: "Default" }, ...SYMBOL_FILL_COLORS].map((item) => ({
     ...item,
     sidc: "100" + props.sid + 10 + "00" + "00" + "0000000000",
     symbolOptions: item.code
@@ -46,3 +26,7 @@ const colorIconItems = computed((): NullableSymbolItem[] =>
   })),
 );
 </script>
+
+<template>
+  <SymbolCodeSelect label="Fill color" :items="colorIconItems" v-model="colorValue" />
+</template>
