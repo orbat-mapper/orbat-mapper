6aa1ca4413d0a80ab4eea4612f31525223a727dd
Kjell Magne Fauske <kjellmf@gmail.com>
2025-11-02 14:38:10 +0100

    Add Card and RadioGroup components from shadcn

---
+++ src/components/ui/radio-group/RadioGroupItem.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { RadioGroupItemProps } from "reka-ui";
+import type { HTMLAttributes } from "vue";
+import { reactiveOmit } from "@vueuse/core";
+import { Check } from "lucide-vue-next";
+import { RadioGroupIndicator, RadioGroupItem, useForwardProps } from "reka-ui";
+import { cn } from "@/lib/utils";
+
+const props = defineProps<RadioGroupItemProps & { class?: HTMLAttributes["class"] }>();
+
+const delegatedProps = reactiveOmit(props, "class");
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <RadioGroupItem
+    v-bind="forwardedProps"
+    :class="
+      cn(
+        'peer border-primary text-primary focus-visible:ring-ring aspect-square h-4 w-4 rounded-full border shadow focus:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
+        props.class,
+      )
+    "
+  >
+    <RadioGroupIndicator class="flex items-center justify-center">
+      <Check class="text-primary h-3.5 w-3.5" />
+    </RadioGroupIndicator>
+  </RadioGroupItem>
+</template>
