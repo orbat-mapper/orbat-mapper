53459e9adf8630476fbab87cb466b741ef62eac5
KM <kjellmf@gmail.com>
2025-11-21 06:14:27 +0100

    Put debug info in a separate component

---
+++ src/components/DebugInfo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+<template>
+  <div
+    class="bg-opacity-70 bg-background text-foreground fixed right-10 bottom-2 z-50 rounded print:hidden"
+  >
+    <p class="p-2 text-xs font-bold">
+      <span class="sm:hidden">mo</span>
+      <span class="hidden sm:inline md:hidden">sm</span>
+      <span class="hidden md:inline lg:hidden">md</span>
+      <span class="hidden lg:inline xl:hidden">lg</span>
+      <span class="hidden xl:inline 2xl:hidden">xl</span>
+      <span class="3xl:hidden hidden 2xl:inline">2xl</span>
+      <span class="3xl:inline 4xl:hidden hidden">3xl</span>
+      <span class="4xl:inline hidden">4xl+</span>
+    </p>
+
+    <p></p>
+  </div>
+</template>
