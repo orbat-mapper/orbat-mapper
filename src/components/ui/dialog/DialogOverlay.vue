cd0515249857c1731ff64e71f6dc172688f85ea4
Kjell Magne Fauske <kjellmf@gmail.com>
2025-12-19 05:24:09 +0100

    Update Dialog component from shadcn-vue
    Revert placement of DialogScrollContent

--- src/components/ui/dialog/DialogOverlay.vue
+++ src/components/ui/dialog/DialogOverlay.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
+import type { DialogOverlayProps } from "reka-ui";
+import type { HTMLAttributes } from "vue";
+import { reactiveOmit } from "@vueuse/core";
+import { DialogOverlay } from "reka-ui";
 import { cn } from "@/lib/utils";
-import { DialogOverlay, type DialogOverlayProps } from "reka-ui";
-import { computed, type HTMLAttributes } from "vue";
 
 const props = defineProps<DialogOverlayProps & { class?: HTMLAttributes["class"] }>();
 
-const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props;
-
-  return delegated;
-});
+const delegatedProps = reactiveOmit(props, "class");
 </script>
 
 <template>
