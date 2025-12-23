9f36c425b930704a729714192391110b7d53d02e
Kjell Magne Fauske <kjellmf@gmail.com>
2025-12-23 08:31:09 +0100

    Add Item component from shadcn-vue

---
+++ src/components/ui/item/ItemSeparator.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { SeparatorProps } from "reka-ui";
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+
+const props = defineProps<SeparatorProps & { class?: HTMLAttributes["class"] }>();
+</script>
+
+<template>
+  <Separator
+    data-slot="item-separator"
+    orientation="horizontal"
+    :class="cn('my-0', props.class)"
+  />
+</template>
