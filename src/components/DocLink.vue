80cbc0d8e9995125e55d69468430449b9869be43
Kjell Magne Fauske <kjellmf@gmail.com>
2025-12-30 07:02:21 +0100

    Add icon to DocLink component

--- src/components/DocLink.vue
+++ src/components/DocLink.vue
@@ -1,8 +1,11 @@
-<template>
-  <Button as-child variant="link"
-    ><a target="_blank" class=""> <slot>Help</slot> &nearr;</a></Button
-  >
-</template>
 <script setup lang="ts">
 import { Button } from "@/components/ui/button";
+import { ExternalLinkIcon } from "lucide-vue-next";
 </script>
+
+<template>
+  <Button as-child variant="link"
+    ><a target="_blank">
+      <slot>Help</slot><ExternalLinkIcon class="text-muted-foreground -ml-1" /></a
+  ></Button>
+</template>
