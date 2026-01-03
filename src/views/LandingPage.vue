05075a20f9ddaa35f86876d7aa472314627ae1bf
Kjell Magne Fauske <kjellmf@gmail.com>
2026-01-03 07:09:11 +0100

    Relocate Globe view link to a more prominent position on LandingPage.

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -103,6 +103,7 @@
               <router-link :to="{ name: TEXT_TO_ORBAT_ROUTE }" class="shrink-0"
                 >Text to ORBAT
               </router-link>
+              <router-link to="/globe/demo-falkland82" class="">Globe view</router-link>
 
               <router-link :to="{ name: ORBAT_CHART_ROUTE }" class="text-muted-foreground"
                 >Orbat chart test
@@ -117,7 +118,6 @@
               <router-link to="/testgrid2" class="text-muted-foreground"
                 >Tanstack Table Test grid</router-link
               >
-              <router-link to="/globe/demo-falkland82" class="">Globe view</router-link>
             </ul>
           </div>
         </ProseSection>
