374bdbf1aa573b90bf89d096a6782b5290b7a79c
KM <kjellmf@gmail.com>
2026-04-01 07:29:03 +0200

    Add missing short name input to unit details form with auto mode

--- src/modules/scenarioeditor/EditMetaForm.vue
+++ src/modules/scenarioeditor/EditMetaForm.vue
@@ -4,6 +4,23 @@ import { computed, defineAsyncComponent, ref, watch } from "vue";
 import BaseButton from "@/components/BaseButton.vue";
 import { klona } from "klona";
 import type { NScenarioEvent, NScenarioFeature, NUnit } from "@/types/internalModels";
+import { createShortUnitName } from "@/utils/shortUnitName";
+import { useTextToOrbatStore } from "@/views/texttoorbat/textToOrbatStore";
+import { storeToRefs } from "pinia";
+import { WandSparklesIcon, ChevronDownIcon } from "lucide-vue-next";
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuCheckboxItem,
+  DropdownMenuSeparator,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from "@/components/ui/dropdown-menu";
 
 const SimpleMarkdownInput = defineAsyncComponent(
   () => import("@/components/SimpleMarkdownInput.vue"),
@@ -47,7 +64,7 @@ const isUnitType = (item: NUnit | NScenarioFeature | NScenarioEvent): item is NU
 };
 
 const isUnit = computed(() => {
-  return props.item && !isUnitType(props.item);
+  return props.item && isUnitType(props.item);
 });
 
 const isScenarioEvent = computed(() => {
@@ -83,6 +100,26 @@ watch(
   { immediate: true },
 );
 
+const {
+  shortNameMaxLength,
+  shortNameUppercase,
+  shortNameAllowWhitespace,
+  shortNameForceLength,
+} = storeToRefs(useTextToOrbatStore());
+
+function generateShortName() {
+  if (!form.value.name) return;
+  const result = createShortUnitName(form.value.name, {
+    maxLength: shortNameMaxLength.value,
+    uppercase: shortNameUppercase.value,
+    allowWhitespace: shortNameAllowWhitespace.value,
+    forceLength: shortNameForceLength.value,
+  });
+  if (result) {
+    form.value.shortName = result;
+  }
+}
+
 const onFormSubmit = () => {
   emit("update", klona(form.value));
 };
@@ -96,12 +133,78 @@ const onFormSubmit = () => {
 
     <template v-else>
       <InputGroup label="Name" v-model="form.name" id="name-input" autofocus />
-      <InputGroup
-        v-if="isUnit"
-        label="Short name"
-        description="Alternative name"
-        v-model="form.shortName"
-      />
+      <Field v-if="isUnit">
+        <FieldLabel>Short name</FieldLabel>
+        <div class="flex items-center gap-1">
+          <Input type="text" v-model="form.shortName" class="flex-1" />
+          <Button
+            type="button"
+            variant="outline"
+            size="icon"
+            class="shrink-0"
+            title="Generate short name"
+            @click="generateShortName()"
+          >
+            <WandSparklesIcon class="size-4" />
+          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger as-child>
+              <Button
+                type="button"
+                variant="outline"
+                size="icon"
+                class="shrink-0"
+                title="Short name settings"
+              >
+                <ChevronDownIcon class="size-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              class="min-w-56"
+              @pointer-down-outside="$event.detail.originalEvent.stopPropagation()"
+            >
+              <DropdownMenuCheckboxItem v-model="shortNameUppercase" @select.prevent>
+                Uppercase output
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem
+                v-model="shortNameAllowWhitespace"
+                @select.prevent
+              >
+                Allow whitespace
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem v-model="shortNameForceLength" @select.prevent>
+                Force length
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuLabel>Max length</DropdownMenuLabel>
+              <DropdownMenuRadioGroup
+                :model-value="String(shortNameMaxLength)"
+                @update:model-value="shortNameMaxLength = Number($event)"
+              >
+                <DropdownMenuRadioItem value="3" @select.prevent>
+                  3
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="4" @select.prevent>
+                  4
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="6" @select.prevent>
+                  6
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="8" @select.prevent>
+                  8
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="10" @select.prevent>
+                  10
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="12" @select.prevent>
+                  12
+                </DropdownMenuRadioItem>
+              </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <FieldDescription>Alternative name</FieldDescription>
+      </Field>
     </template>
     <SimpleMarkdownInput
       label="Description"
